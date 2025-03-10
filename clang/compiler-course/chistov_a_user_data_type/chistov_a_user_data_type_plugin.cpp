392d740df2c38b4c59ab21a0dcf5925c3fcc666d
wabka22 <alexey.chistov10@gmail.com>
2025-03-10 09:22:39 +0300

    working version

--- clang/compiler-course/chistov_a_user_data_type/chistov_a_user_data_type_plugin.cpp
+++ clang/compiler-course/chistov_a_user_data_type/chistov_a_user_data_type_plugin.cpp
@@ -5,9 +5,11 @@
 #include "llvm/Support/raw_ostream.h"
 
 namespace {
-class Printer {
-  std::string GetAccessSpecifier(const clang::ValueDecl *member) {
-    switch (member->getAccess()) {
+class PrintDataVisitor final : public clang::RecursiveASTVisitor<PrintDataVisitor> {
+  clang::ASTContext *class_context_;
+
+  std::string AccessSpecifierToString(clang::AccessSpecifier accessSpecifier) {
+    switch (accessSpecifier) {
       case clang::AS_public:
         return "public";
       case clang::AS_protected:
@@ -15,26 +17,18 @@ class Printer {
       case clang::AS_private:
         return "private";
       default:
-        llvm::errs() << "Error: Unknown access specifier for member: "
-                     << member->getNameAsString() << "\n";
+        llvm::errs() << "Error: Unknown access specifier\n";
         return "unknown access specifier";
     }
   }
 
-public:
-  void PrintType(clang::CXXRecordDecl *userType) {
-    llvm::outs() << userType->getNameAsString() << (userType->isStruct() ? "(struct" : "(class")
-                 << (userType->isTemplated() ? "|template)" : ")") << '\n';
-  }
-
   void PrintMember(const clang::ValueDecl *member, const std::string &member_type) {
+    llvm::outs() << "| |_ " << member->getNameAsString() << ' ';
     if (member_type == "method") {
-      llvm::outs() << "| |_ " << member->getNameAsString() << ' ';
       llvm::outs() << '(';
       if (const auto *method = llvm::dyn_cast<clang::CXXMethodDecl>(member)) {
         llvm::outs() << method->getReturnType().getAsString();
-        llvm::outs() << '|';
-        llvm::outs() << GetAccessSpecifier(member);
+        llvm::outs() << '|' << AccessSpecifierToString(member->getAccess());
         if (method->isVirtual()) {
           llvm::outs() << "|virtual";
         }
@@ -46,51 +40,37 @@ public:
         }
       }
     } else {
-      llvm::outs() << "| |_ " << member->getNameAsString() << ' ';
-      llvm::outs() << '(' << member->getType().getAsString() << '|';
-      llvm::outs() << GetAccessSpecifier(member);
+      llvm::outs() << '(' << member->getType().getAsString() << '|'
+                   << AccessSpecifierToString(member->getAccess());
     }
     llvm::outs() << ")\n";
   }
-};
 
-class PrintDataVisitor final : public clang::RecursiveASTVisitor<PrintDataVisitor> {
 public:
   explicit PrintDataVisitor(clang::ASTContext *context) : class_context_(context) {}
 
   bool VisitCXXRecordDecl(clang::CXXRecordDecl *declaration) {
-    printer_.PrintType(declaration);
+    llvm::outs() << declaration->getNameAsString()
+                 << (declaration->isStruct() ? "(struct" : "(class")
+                 << (declaration->isTemplated() ? "|template)" : ")") << '\n';
+
     if (!declaration->bases().empty()) {
       for (const auto &base : declaration->bases()) {
         const clang::CXXRecordDecl *baseDecl = base.getType()->getAsCXXRecordDecl();
         if (baseDecl) {
           clang::AccessSpecifier accessSpecifier = base.getAccessSpecifier();
-          std::string access;
-          switch (accessSpecifier) {
-            case clang::AS_public:
-              access = "public";
-              break;
-            case clang::AS_protected:
-              access = "protected";
-              break;
-            case clang::AS_private:
-              access = "private";
-              break;
-            default:
-              access = "unknown";
-              break;
-          }
           llvm::outs() << declaration->getName() << " -> "
-                       << access << " " << baseDecl->getName() << "\n";
+                       << AccessSpecifierToString(accessSpecifier) << " "
+                       << baseDecl->getName() << "\n";
         }
       }
     }
 
     if (!declaration->field_empty()) {
       llvm::outs() << "|_Fields\n";
-      for (const auto &decl : declaration->decls()) {
+      for (const auto *decl : declaration->decls()) {
         if (auto *field = llvm::dyn_cast<clang::FieldDecl>(decl)) {
-          printer_.PrintMember(field, "field");
+          PrintMember(field, "field");
         }
       }
     }
@@ -98,16 +78,12 @@ public:
     if (!declaration->methods().empty()) {
       llvm::outs() << "|_Methods\n";
       for (const auto *method : declaration->methods()) {
-        printer_.PrintMember(method, "method");
+        PrintMember(method, "method");
       }
     }
     llvm::outs() << '\n';
     return true;
   }
-
-private:
-  clang::ASTContext *class_context_;
-  Printer printer_;
 };
 
 class PrintDataConsumer final : public clang::ASTConsumer {
@@ -123,7 +99,7 @@ private:
 };
 
 class PrintDataAction final : public clang::PluginASTAction {
- public:
+public:
   std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
     return std::make_unique<PrintDataConsumer>(&ci.getASTContext());
   }
