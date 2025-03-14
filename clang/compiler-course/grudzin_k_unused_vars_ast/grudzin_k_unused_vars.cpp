9b2c9e8a1af151d8b1bbbf3ef1986fb3b1a6a013
Konstantin-Grudzin <grudzinkostya@yandex.kz>
2025-03-14 09:29:14 +0300

    clang-format-fix

--- clang/compiler-course/grudzin_k_unused_vars_ast/grudzin_k_unused_vars.cpp
+++ clang/compiler-course/grudzin_k_unused_vars_ast/grudzin_k_unused_vars.cpp
@@ -6,21 +6,22 @@
 #include "llvm/Support/raw_ostream.h"
 
 namespace {
-class MyUnVarsVisitor final : public clang::RecursiveASTVisitor<MyUnVarsVisitor> {
-public:
-  explicit MyUnVarsVisitor(clang::ASTContext *context) : m_context(context) {}
+  class MyUnVarsVisitor final
+      : public clang::RecursiveASTVisitor<MyUnVarsVisitor> {
+   public:
+     explicit MyUnVarsVisitor(clang::ASTContext *context) : m_context(context) {}
 
-  //(check https://compiler-explorer.com/z/M3vxY6P15 for correct type)
+  // (check https://compiler-explorer.com/z/M3vxY6P15 for correct type)
   bool VisitParamVarDecl(clang::ParmVarDecl *parametr) {
     if(!parametr->isUsed() && !parametr->hasAttr<clang::UnusedAttr>()){
         parametr->addAttr(clang::UnusedAttr::Create(*m_context));
-   }
+    }
     return true;
   }
   bool VisitVarDecl(clang::VarDecl *variable) {
     if(!variable->isUsed() && !variable->hasAttr<clang::UnusedAttr>()){
         variable->addAttr(clang::UnusedAttr::Create(*m_context));
-  }
+    }
     return true;
   }
 
@@ -28,21 +29,22 @@ private:
   clang::ASTContext *m_context;
 };
 
-//You need to dump your data to change code info
+// You need to dump your data to change code info
 class MyDumpVisitor final : public clang::RecursiveASTVisitor<MyDumpVisitor> {
-  public:
-    explicit MyDumpVisitor(clang::ASTContext *context) : m_context(context) {}
-    bool VisitFunctionDecl(clang::FunctionDecl *function) {
-      function->dump();
-      return true;
-    }
-  private:
-    clang::ASTContext *m_context;
-  };
+public:
+  explicit MyDumpVisitor(clang::ASTContext *context) : m_context(context) {}
+  bool VisitFunctionDecl(clang::FunctionDecl *function) {
+    function->dump();
+    return true;
+  }
+private:
+  clang::ASTContext *m_context;
+};
 
 class MyUnVarsConsumer final : public clang::ASTConsumer {
 public:
-  explicit MyUnVarsConsumer(clang::ASTContext *context) : m_visitor(context),d_visitor(context) {}
+  explicit MyUnVarsConsumer(clang::ASTContext *context)
+       : m_visitor(context),d_visitor(context) {}
 
   void HandleTranslationUnit(clang::ASTContext &context) override {
     m_visitor.TraverseDecl(context.getTranslationUnitDecl());
@@ -50,8 +52,8 @@ public:
   }
 
 private:
-MyUnVarsVisitor m_visitor;
-MyDumpVisitor d_visitor;
+  MyUnVarsVisitor m_visitor;
+  MyDumpVisitor d_visitor;
 };
 
 class MyUnVarsAction final : public clang::PluginASTAction {
