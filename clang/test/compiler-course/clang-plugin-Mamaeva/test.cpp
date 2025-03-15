8baaed39e959083626a70fbd21ddcd6740cf9de9
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-03-15 11:20:24 +0000

    Delete clang/test/compiler-course/clang-plugin-Mamaeva directory

--- clang/test/compiler-course/clang-plugin-Mamaeva/test.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: clang -Xclang -load -Xclang ./libmyClangPlugin_mamaeva_olga_FIIT3_ClangAST.so -Xclang -plugin -Xclang myClangPlugin -c %s
-
-double sum(int a, float b) {
-    return a + b;
-}
-
-int mul(float a, float b) {
-    return a + sum(a, b);
-}
