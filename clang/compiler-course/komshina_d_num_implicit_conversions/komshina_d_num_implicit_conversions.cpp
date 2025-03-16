e1a06e0acf0227a9bbcba74b3e54bd5160e33bb7
MikasaAkc <daria.komshina@mail.ru>
2025-03-16 10:52:30 +0300

    clang

--- clang/compiler-course/komshina_d_num_implicit_conversions/komshina_d_num_implicit_conversions.cpp
+++ clang/compiler-course/komshina_d_num_implicit_conversions/komshina_d_num_implicit_conversions.cpp
@@ -10,119 +10,103 @@
 
 namespace {
 
-	class ImplicitConversionsAnalyzer final
-		 : public clang::RecursiveASTVisitor<ImplicitConversionsAnalyzer> {
-	public:
-		explicit ImplicitConversionsAnalyzer() {}
+class ImplicitConversionsAnalyzer final
+    : public clang::RecursiveASTVisitor<ImplicitConversionsAnalyzer> {
+public:
+  explicit ImplicitConversionsAnalyzer() {}
+
+  bool VisitFunctionDecl(clang::FunctionDecl *func) {
+    CurrentFunction = func->getNameAsString();
+    return true;
+  }
+
+  bool VisitCXXConstructExpr(clang::CXXConstructExpr *Conv) {
+    if (Conv->getNumArgs() == 0)
+      return true;
+    HandleConv(Conv->getArg(0)->getType(), Conv->getType());
+    return true;
+  }
+
+  bool VisitImplicitCastExpr(clang::ImplicitCastExpr *CastExpr) {
+    switch (CastExpr->getCastKind()) {
+    case clang::CK_NoOp:
+    case clang::CK_LValueToRValue:
+    case clang::CK_FunctionToPointerDecay:
+      return true;
+    default:
+      HandleConv(CastExpr->getSubExpr()->getType(), CastExpr->getType());
+      return true;
+    }
+  }
+
+  void PrintReport(bool includeTotal = false) const {
+    std::set<std::string> processedFunctions;
+    for (const auto &entry : CastList) {
+      const auto &[FunctionName, FromType, ToType] = entry;
+
+      if (processedFunctions.find(FunctionName) == processedFunctions.end()) {
+        llvm::outs() << "CHECK: Function " << FunctionName << "\n";
+        processedFunctions.insert(FunctionName);
+      }
+
+      llvm::outs() << "CHECK-NEXT: " << FromType + " -> " + ToType << ": 1\n";
+    }
+  }
 		
-			bool VisitFunctionDecl(clang::FunctionDecl * func) {
-			CurrentFunction = func->getNameAsString();
-			return true;
-			
-		}
-		
-			bool VisitCXXConstructExpr(clang::CXXConstructExpr * Conv) {
-			if (Conv->getNumArgs() == 0)
-				 return true;
-			HandleConv(Conv->getArg(0)->getType(), Conv->getType());
-			return true;
-			
-		}
-		
-			bool VisitImplicitCastExpr(clang::ImplicitCastExpr * CastExpr) {
-			switch (CastExpr->getCastKind()) {
-			case clang::CK_NoOp:
-			case clang::CK_LValueToRValue:
-			case clang::CK_FunctionToPointerDecay:
-				return true;
-			default:
-				HandleConv(CastExpr->getSubExpr()->getType(), CastExpr->getType());
-				return true;
-				
-			}
-			
-		}
-		
-			void PrintReport(bool includeTotal = false) const {
-			std::set<std::string> processedFunctions;
-			for (const auto& entry : CastList) {
-				const auto & [FunctionName, FromType, ToType] = entry;
-				
-					if (processedFunctions.find(FunctionName) == processedFunctions.end()) {
-					llvm::outs() << "CHECK: Function " << FunctionName << "\n";
-					processedFunctions.insert(FunctionName);
-					
-				}
-				
-					llvm::outs() << "CHECK-NEXT: " << FromType + " -> " + ToType << ": 1\n";
-				
-			}
-			
-		}
-		
-	private:
-		void HandleConv(clang::QualType From, clang::QualType To) {
-			From = ResolveType(From);
-			To = ResolveType(To);
-			if (From == To)
-				 return;
-			
-				CastList.emplace_back(CurrentFunction, From.getAsString(),
-					To.getAsString());
-			
-		}
-		
-			clang::QualType ResolveType(clang::QualType Type) {
-			Type = Type.getCanonicalType();
-			if (auto* TST = Type->getAs<clang::TypedefType>()) {
-				return TST->getDecl()->getUnderlyingType().getCanonicalType();
-				
-			}
-			 if (auto* ET = Type->getAs<clang::ElaboratedType>()) {
-				return ET->getNamedType().getCanonicalType();
-				
-			}
-			 return Type;
-			
-		}
-		
-			std::string CurrentFunction;
-		std::vector<std::tuple<std::string, std::string, std::string>> CastList;
-		};
+private:
+  void HandleConv(clang::QualType From, clang::QualType To) {
+    From = ResolveType(From);
+    To = ResolveType(To);
+    if (From == To)
+      return;
+
+    CastList.emplace_back(CurrentFunction, From.getAsString(),
+                          To.getAsString());
+  }
+
+  clang::QualType ResolveType(clang::QualType Type) {
+    Type = Type.getCanonicalType();
+    if (auto *TST = Type->getAs<clang::TypedefType>()) {
+      return TST->getDecl()->getUnderlyingType().getCanonicalType();
+    }
+    if (auto *ET = Type->getAs<clang::ElaboratedType>()) {
+      return ET->getNamedType().getCanonicalType();
+    }
+    return Type;
+  }
+
+  std::string CurrentFunction;
+  std::vector<std::tuple<std::string, std::string, std::string>> CastList;
+};
 	
-		class ImplicitConversionsConsumer final : public clang::ASTConsumer {
-		public:
-			ImplicitConversionsConsumer() {}
-			
-				void HandleTranslationUnit(clang::ASTContext & context) override {
-				m_visitor.TraverseDecl(context.getTranslationUnitDecl());
-				m_visitor.PrintReport(true);
-				
-			}
-			
-		private:
-			ImplicitConversionsAnalyzer m_visitor;
-			
-	};
-	
-		class ImplicitConversionsAction final : public clang::PluginASTAction {
-		public:
-			std::unique_ptr<clang::ASTConsumer>
-				CreateASTConsumer(clang::CompilerInstance & ci, llvm::StringRef) override {
-				return std::make_unique<ImplicitConversionsConsumer>();
-				
-			}
-			
-				bool ParseArgs(const clang::CompilerInstance & ci,
-					const std::vector<std::string> &args) override {
-				return true;
-				
-			}
-			
-	};
+class ImplicitConversionsConsumer final : public clang::ASTConsumer {
+public:
+  ImplicitConversionsConsumer() {}
+
+  void HandleTranslationUnit(clang::ASTContext &context) override {
+    m_visitor.TraverseDecl(context.getTranslationUnitDecl());
+    m_visitor.PrintReport(true);
+  }
+
+private:
+  ImplicitConversionsAnalyzer m_visitor;
+};
+
+class ImplicitConversionsAction final : public clang::PluginASTAction {
+public:
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
+    return std::make_unique<ImplicitConversionsConsumer>();
+  }
+
+  bool ParseArgs(const clang::CompilerInstance &ci,
+                 const std::vector<std::string> &args) override {
+    return true;
+  }
+};
 
 } // namespace
 
 static clang::FrontendPluginRegistry::Add<ImplicitConversionsAction>
-X("Implicit_Conversions_Komshina_Daria_FIIT1_ClangAST",
-	"Count implicit type conversions");
+    X("Implicit_Conversions_Komshina_Daria_FIIT1_ClangAST",
+      "Count implicit type conversions");
