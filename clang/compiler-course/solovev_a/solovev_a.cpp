85ed422472471b3c5f6a7cd95d27c7bae9abe59e
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-03-16 20:31:31 +0300

    clang

--- clang/compiler-course/solovev_a/solovev_a.cpp
+++ clang/compiler-course/solovev_a/solovev_a.cpp
@@ -9,93 +9,95 @@
 
 namespace {
 
-    class PrefixVariableNames : public clang::RecursiveASTVisitor<PrefixVariableNames> {
-    public:
-        explicit PrefixVariableNames(clang::Rewriter& rewriter) : rewriter_(rewriter) {}
+class PrefixVariableNames : public clang::RecursiveASTVisitor<PrefixVariableNames> {
+public:
+  explicit PrefixVariableNames(clang::Rewriter& rewriter)
+      : rewriter_(rewriter) {}
 
-        bool VisitVarDecl(clang::VarDecl* varDecl) {
-            if (!varDecl->getName().empty()) {
-                std::string prefix = determinePrefix(varDecl);
-                if (!prefix.empty() && varDecl->getName().str().find(prefix) != 0) {
-                    std::string name = varDecl->getName().str();
-                    std::string mod_name = prefix + name;
-                    renamedVariables_[name] = mod_name;
-                    rewriter_.ReplaceText(varDecl->getLocation(), name.length(), mod_name);
-                }
-            }
-            return true;
-        }
+  bool VisitVarDecl(clang::VarDecl* varDecl) {
+    if (!varDecl->getName().empty()) {
+      std::string prefix = determinePrefix(varDecl);
+      if (!prefix.empty() && varDecl->getName().str().find(prefix) != 0) {
+        std::string name = varDecl->getName().str();
+        std::string mod_name = prefix + name;
+        renamedVariables_[name] = mod_name;
+        rewriter_.ReplaceText(varDecl->getLocation(), name.length(), mod_name);
+      }
+    }
+    return true;
+  }
 
-        bool VisitParmVarDecl(clang::ParmVarDecl* paramVarDecl) {
-            if (!paramVarDecl->getName().empty()) {
-                std::string name = paramVarDecl->getName().str();
-                std::string mod_name = "param_" + name;
-                renamedVariables_[name] = mod_name;
-                rewriter_.ReplaceText(paramVarDecl->getLocation(), name.length(), mod_name);
-            }
-            return true;
-        }
+  bool VisitParmVarDecl(clang::ParmVarDecl* paramVarDecl) {
+    if (!paramVarDecl->getName().empty()) {
+      std::string name = paramVarDecl->getName().str();
+      std::string mod_name = "param_" + name;
+      renamedVariables_[name] = mod_name;
+      rewriter_.ReplaceText(paramVarDecl->getLocation(), name.length(), mod_name);
+    }
+    return true;
+  }
 
-        bool VisitDeclRefExpr(clang::DeclRefExpr* expr) {
-            auto decl = expr->getDecl();
-            if (decl) {
-                std::string name = decl->getName().str();
-                auto it = renamedVariables_.find(name);
-                if (it != renamedVariables_.end()) {
-                    rewriter_.ReplaceText(expr->getLocation(), name.length(), it->second);
-                }
-            }
-            return true;
-        }
+  bool VisitDeclRefExpr(clang::DeclRefExpr* expr) {
+    auto decl = expr->getDecl();
+    if (decl) {
+      std::string name = decl->getName().str();
+      auto it = renamedVariables_.find(name);
+      if (it != renamedVariables_.end()) {
+        rewriter_.ReplaceText(expr->getLocation(), name.length(), it->second);
+      }
+    }
+    return true;
+  }
 
-    private:
-        std::string determinePrefix(clang::VarDecl* varDecl) {
-            if (varDecl->isStaticLocal()) {
-                return "static_";
-            }
-            else if (varDecl->isLocalVarDecl()) {
-                return "local_";
-            }
-            else if (varDecl->hasGlobalStorage()) {
-                return "global_";
-            }
-            return "";
-        }
+private:
+  std::string determinePrefix(clang::VarDecl* varDecl) {
+    if (varDecl->isStaticLocal()) {
+      return "static_";
+    } else if (varDecl->isLocalVarDecl()) {
+      return "local_";
+    } else if (varDecl->hasGlobalStorage()) {
+      return "global_";
+    }
+    return "";
+  }
 
-        clang::Rewriter& rewriter_;
-        std::unordered_map<std::string, std::string> renamedVariables_;
-    };
+  clang::Rewriter& rewriter_;
+  std::unordered_map<std::string, std::string> renamedVariables_;
+};
 
-    class PrefixConsumer : public clang::ASTConsumer {
-    public:
-        explicit PrefixConsumer(clang::Rewriter& rewriter) : visitor_(rewriter) {}
+class PrefixConsumer : public clang::ASTConsumer {
+public:
+  explicit PrefixConsumer(clang::Rewriter& rewriter) : visitor_(rewriter) {}
 
-        void HandleTranslationUnit(clang::ASTContext& context) override {
-            visitor_.TraverseDecl(context.getTranslationUnitDecl());
-        }
+  void HandleTranslationUnit(clang::ASTContext& context) override {
+    visitor_.TraverseDecl(context.getTranslationUnitDecl());
+  }
 
-    private:
-        PrefixVariableNames visitor_;
-    };
+private:
+  PrefixVariableNames visitor_;
+};
 
-    class PrefixAction : public clang::PluginASTAction {
-    public:
-        std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance& ci, llvm::StringRef) override {
-            rewriter_.setSourceMgr(ci.getSourceManager(), ci.getLangOpts());
-            return std::make_unique<PrefixConsumer>(rewriter_);
-        }
+class PrefixAction : public clang::PluginASTAction {
+public:
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance& ci, llvm::StringRef) override {
+    rewriter_.setSourceMgr(ci.getSourceManager(), ci.getLangOpts());
+    return std::make_unique<PrefixConsumer>(rewriter_);
+  }
 
-        bool ParseArgs(const clang::CompilerInstance&, const std::vector<std::string>&) override {
-            return true;
-        }
+  bool ParseArgs(const clang::CompilerInstance &,
+                 const std::vector<std::string> &) override {
+    return true;
+  }
 
-        void EndSourceFileAction() override {
-            rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID()).write(llvm::outs());
-        }
+  void EndSourceFileAction() override {
+    rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
+  }
 
-    private:
-        clang::Rewriter rewriter_;
-    };
+private:
+  clang::Rewriter rewriter_;
+};
 
 } // namespace
 
