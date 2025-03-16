b792e47e7248ac811757ebfe8d68e4506b65f8c8
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-03-16 20:42:44 +0300

    clang

--- clang/compiler-course/solovev_a/solovev_a.cpp
+++ clang/compiler-course/solovev_a/solovev_a.cpp
@@ -4,17 +4,18 @@
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "llvm/Support/raw_ostream.h"
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 namespace {
 
-class PrefixVariableNames : public clang::RecursiveASTVisitor<PrefixVariableNames> {
+class PrefixVariableNames
+    : public clang::RecursiveASTVisitor<PrefixVariableNames> {
 public:
-  explicit PrefixVariableNames(clang::Rewriter& rewriter)
+  explicit PrefixVariableNames(clang::Rewriter &rewriter)
       : rewriter_(rewriter) {}
 
-  bool VisitVarDecl(clang::VarDecl* varDecl) {
+  bool VisitVarDecl(clang::VarDecl *varDecl) {
     if (!varDecl->getName().empty()) {
       std::string prefix = determinePrefix(varDecl);
       if (!prefix.empty() && varDecl->getName().str().find(prefix) != 0) {
@@ -27,7 +28,7 @@ public:
     return true;
   }
 
-  bool VisitParmVarDecl(clang::ParmVarDecl* paramVarDecl) {
+  bool VisitParmVarDecl(clang::ParmVarDecl *paramVarDecl) {
     if (!paramVarDecl->getName().empty()) {
       std::string name = paramVarDecl->getName().str();
       std::string mod_name = "param_" + name;
@@ -37,7 +38,7 @@ public:
     return true;
   }
 
-  bool VisitDeclRefExpr(clang::DeclRefExpr* expr) {
+  bool VisitDeclRefExpr(clang::DeclRefExpr *expr) {
     auto decl = expr->getDecl();
     if (decl) {
       std::string name = decl->getName().str();
@@ -50,7 +51,7 @@ public:
   }
 
 private:
-  std::string determinePrefix(clang::VarDecl* varDecl) {
+  std::string determinePrefix(clang::VarDecl *varDecl) {
     if (varDecl->isStaticLocal()) {
       return "static_";
     } else if (varDecl->isLocalVarDecl()) {
@@ -61,15 +62,15 @@ private:
     return "";
   }
 
-  clang::Rewriter& rewriter_;
+  clang::Rewriter &rewriter_;
   std::unordered_map<std::string, std::string> renamedVariables_;
 };
 
 class PrefixConsumer : public clang::ASTConsumer {
 public:
-  explicit PrefixConsumer(clang::Rewriter& rewriter) : visitor_(rewriter) {}
+  explicit PrefixConsumer(clang::Rewriter &rewriter) : visitor_(rewriter) {}
 
-  void HandleTranslationUnit(clang::ASTContext& context) override {
+  void HandleTranslationUnit(clang::ASTContext &context) override {
     visitor_.TraverseDecl(context.getTranslationUnitDecl());
   }
 
@@ -80,7 +81,7 @@ private:
 class PrefixAction : public clang::PluginASTAction {
 public:
   std::unique_ptr<clang::ASTConsumer>
-  CreateASTConsumer(clang::CompilerInstance& ci, llvm::StringRef) override {
+  CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
     rewriter_.setSourceMgr(ci.getSourceManager(), ci.getLangOpts());
     return std::make_unique<PrefixConsumer>(rewriter_);
   }
@@ -102,4 +103,5 @@ private:
 } // namespace
 
 static clang::FrontendPluginRegistry::Add<PrefixAction>
-X("ClangAST_1_Solovev_a_FIIT1_ClangAST", "Prefixes variables based on type");
+    X("ClangAST_1_Solovev_a_FIIT1_ClangAST",
+      "Prefixes variables based on type");
