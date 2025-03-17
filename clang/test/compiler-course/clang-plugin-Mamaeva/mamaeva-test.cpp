bd77a0404fce374060864dc9e111fdb51881ac9d
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-03-17 10:41:41 +0000

    Update mamaeva-test.cpp

--- clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
+++ clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
@@ -1,4 +1,8 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_Mamaeva_Olga_FIIT3_ClangAST%pluginext -plugin myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s -dump-input=always
+// RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_mamaeva_olga_FIIT3_ClangAST%pluginext -plugin myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s -dump-input=always
+
+// CHECK: In testing
+// CHECK-NEXT: int -> float: 1
+float testing = 7;
 
 // CHECK: In function: sum
 // CHECK-NEXT: int -> float: 1
