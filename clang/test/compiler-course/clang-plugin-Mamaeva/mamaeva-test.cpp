503d0195e348afa5172d41e7550e143a23118164
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-03-17 19:01:09 +0300

    Update mamaeva-test.cpp

--- clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
+++ clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
@@ -1,9 +1,6 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_Mamaeva_Olga_FIIT3_ClangAST%pluginext -plugin myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s -dump-input=always
 
 // CHECK: In testing
-// CHECK-NEXT: int -> float: 1
-float testing = 7;
-
 // CHECK: In function: sum
 // CHECK-NEXT: int -> float: 1
 // CHECK-NEXT: float -> double: 1
@@ -14,11 +11,10 @@ double sum(int a, float b) {
 
 // CHECK: In function: mul
 // CHECK-NEXT: float -> double: 1
-// CHECK-NEXT: float -> double: 1
 // CHECK-NEXT: double -> int: 1
 
 int mul(float a, float b) {
     return a + sum(a, b);
 }
 
-// CHECK: Total implicit conversions: 6
+// CHECK: Total implicit conversions: 4
