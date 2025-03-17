e896305fe88be321580398fd914e61f81062171c
dxppi <dxppi@vk.com>
2025-03-17 23:22:54 +0300

    dz1

--- clang/test/compiler-course/plekhanov_unused_variables/test.cpp
+++ clang/test/compiler-course/plekhanov_unused_variables/test.cpp
@@ -1,15 +1,16 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/FindUnused_Plekhanov_Daniil_FIIT2_ClangAST%pluginext -plugin FindUnused -fsyntax-only %s 2>&1 | FileCheck %s
 
-// CHECK: FunctionDecl {{0x[0-9a-fA-F]+}} <{{.*}}> col:20 isEven 'bool (int) noexcept'
-// CHECK-NEXT: |-ParmVarDecl {{0x[0-9a-fA-F]+}} <col:27, col:31> col:31 used value 'int'
-// CHECK-NEXT: |-CompoundStmt {{0x[0-9a-fA-F]+}} <col:47, col:72>
-// CHECK-NEXT: | `-ReturnStmt {{0x[0-9a-fA-F]+}} <col:49, col:69>
-// CHECK-NEXT: |   `-BinaryOperator {{0x[0-9a-fA-F]+}} <col:56, col:69> 'bool' '=='
-// CHECK-NEXT: |     |-BinaryOperator {{0x[0-9a-fA-F]+}} <col:56, col:64> 'int' '%'
-// CHECK-NEXT: |     | |-ImplicitCastExpr {{0x[0-9a-fA-F]+}} <col:56> 'int' <LValueToRValue>
-// CHECK-NEXT: |     | | `-DeclRefExpr {{0x[0-9a-fA-F]+}} <col:56> 'int' lvalue ParmVar {{0x[0-9a-fA-F]+}} 'value' 'int'
-// CHECK-NEXT: |     | `-IntegerLiteral {{0x[0-9a-fA-F]+}} <col:64> 'int' 2
-// CHECK-NEXT: |     `-IntegerLiteral {{0x[0-9a-fA-F]+}} <col:69> 'int' 0
-// CHECK-NEXT: `-WarnUnusedResultAttr {{0x[0-9a-fA-F]+}} <col:3> nodiscard ""
+// CHECK: int baz(int a, int b, \[\[maybe_unused\]\] int c) {
+// CHECK: \[\[maybe_unused\]\] double value=0.0;
+// CHECK: return a + b;
 
-[[nodiscard]] bool isEven(int value) noexcept { return value % 2 == 0; }
+int foo(int a, int b, int c) {
+    double value = 0.0;
+    return a + b;
+}
+
+// CHECK: double foo(double tmp, double tmp1, \[\[maybe_unused\]\] double tmp2) {
+// CHECK: \[\[maybe_unused\]\] double c = tmp + tmp1; 
+double foo(double tmp, double tmp1, double tmp2) {
+	double c = tmp + tmp1; 
+}
