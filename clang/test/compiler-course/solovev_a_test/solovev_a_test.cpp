6b7cfe412b06f7b8b0a32bb9186a1b494256f4cb
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-03-17 13:46:25 +0300

    add tests

--- clang/test/compiler-course/solovev_a_test/solovev_a_test.cpp
+++ clang/test/compiler-course/solovev_a_test/solovev_a_test.cpp
@@ -10,6 +10,17 @@
 // CHECK-NEXT:   int local_d = -param_c;
 // CHECK-NEXT:   return local_d;
 // CHECK-NEXT: }
+// CHECK-NEXT: void func_1(int param_val_1, int param_val_2, int param_val_3) {
+// CHECK-NEXT:   int local_mult = param_val_1 * param_val_2;
+// CHECK-NEXT:   for (int local_i = 0; local_i < local_mult; local_i++) {
+// CHECK-NEXT:     param_val_3 += local_i;
+// CHECK-NEXT:   }
+// CHECK-NEXT: }
+// CHECK-NEXT: void func_2(int param_val_4, int param_val_5) {
+// CHECK-NEXT:   static int static_x = param_val_4 / param_val_5;
+// CHECK-NEXT:   static int static_y = param_val_5 / param_val_4;
+// CHECK-NEXT:   int local_res = static_x + static_y;
+// CHECK-NEXT: }
 
 int a = 0;
 int b = 3;
@@ -20,4 +31,15 @@ int sum(int a, int b) {
 int minus(int c) {
   int d = -c;
   return d;
+}
+void func_1(int val_1, int val_2, int val_3) {
+  int mult = val_1 * val_2;
+  for (int i = 0; i < mult; i++) {
+    val_3 += i;
+  }
+}
+void func_2(int val_4, int val_5) {
+  static int x = val_4 / val_5;
+  static int y = val_5 / val_4;
+  int res = x + y;
 }
