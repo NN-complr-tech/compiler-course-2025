c7f1e4b7717cabb32544236b03c3bbdde41740ae
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-03-17 09:24:09 +0300

    Update mamaeva-test.cpp

--- clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
+++ clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
@@ -1,16 +1,23 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_mamaeva_olga_FIIT3_ClangAST%pluginext -plugin myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_mamaeva_olga_FIIT3_ClangAST%pluginext -plugin myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s -dump-input=always
 
-// CHECK: Function sum
+// CHECK: In testing
 // CHECK-NEXT: int -> float: 1
+float testing = 7;
+
+
+// CHECK: In function: mul
+// CHECK-NEXT: double -> int: 1
 // CHECK-NEXT: float -> double: 1
+// CHECK-NEXT: float -> int: 1
+
+// CHECK: In function: sum
+// CHECK-NEXT: float -> double: 1
+// CHECK-NEXT: int -> float: 1
+
 double sum(int a, float b) {
-    return a + b;
+	return a + b;
 }
 
-// CHECK: Function mul
-// CHECK-NEXT: float -> int: 1
-// CHECK-NEXT: float -> double: 1
-// CHECK-NEXT: double -> int: 1
 int mul(float a, float b) {
-    return a + sum(a, b);
+	return a + sum(a, b);
 }
