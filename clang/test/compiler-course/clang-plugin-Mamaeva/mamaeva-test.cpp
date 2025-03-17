c5fd233c972d288a60224031c13fc4afe78e2543
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-03-17 10:41:16 +0300

    Update mamaeva-test.cpp

--- clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
+++ clang/test/compiler-course/clang-plugin-Mamaeva/mamaeva-test.cpp
@@ -1,4 +1,5 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_mamaeva_olga_FIIT3_ClangAST%pluginext -plugin myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s -dump-input=always
+// RUN: %clang_cc1 -load %llvmshlibdir/myClangPlugin_mamaeva_olga_FIIT3_ClangAST%pluginext -plugin 
+myClangPlugin -fsyntax-only %s 2>&1 | FileCheck %s -dump-input=always
 
 // CHECK: In testing
 // CHECK-NEXT: int -> float: 1
