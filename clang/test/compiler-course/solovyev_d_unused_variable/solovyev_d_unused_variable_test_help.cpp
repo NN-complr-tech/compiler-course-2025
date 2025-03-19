d22fd42ed828b444ac56b65bdbcadec76516a1cb
0xG00SE <61384845+DSolo03@users.noreply.github.com>
2025-03-19 19:43:54 +0300

    delete obsolete test

--- clang/test/compiler-course/solovyev_d_unused_variable/solovyev_d_unused_variable_test_help.cpp
+++
@@ -1,3 +0,0 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/UnusedVariable_Solovyev_Danila_FIIT3_ClangAST%pluginext -plugin unused_variable_plugin -plugin-arg-unused_variable_plugin --help 2>&1 | FileCheck %s
-
-// CHECK:Marking unused variables and function parameters as {{\[\[maybe_unused\]\]}}
