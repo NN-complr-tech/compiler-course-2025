2545951b3ab6e56680c19ac1d4225c9303f3928c
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-03-19 18:38:11 +0300

    Update myClangPlugin.cpp

--- clang/compiler-course/clang-plugin-Mamaeva/myClangPlugin.cpp
+++ clang/compiler-course/clang-plugin-Mamaeva/myClangPlugin.cpp
@@ -124,4 +124,4 @@ public:
 
 // Регистрация плагина
 static clang::FrontendPluginRegistry::Add<MyClangPlugin>
-    X("myClangPlugin", "Counts implicit type conversions");
+    X("ClangAST_1_MamaevaO_FIIT3_ClangAST", "Counts implicit type conversions");
