56c4e5072b71ed6d133b0b828ceb42f5de4162cc
MaxikGuy <S_Maksssim@mail.ru>
2025-03-21 00:37:49 +0300

    removed test with -h flag

--- clang/test/compiler-course/savchenko_m_l1_v2_tests/savchenko_m_l1_v2_test_arg-h.cpp
+++
@@ -1,3 +0,0 @@
-//// -h arg test ////
-// RUN: %clang_cc1 -load %llvmshlibdir/UnusedVariables_Savchenko_Maxim_FIIT1_ClangAST%pluginext -plugin savchenko_m_UnusedVars_plugin -plugin-arg-savchenko_m_UnusedVars_plugin -h 2>&1 | FileCheck --check-prefix=HELP %s
-// HELP: The plugin adds the {{\[\[maybe_unused\]\]}} flag to variables and parameters that are not in use.
