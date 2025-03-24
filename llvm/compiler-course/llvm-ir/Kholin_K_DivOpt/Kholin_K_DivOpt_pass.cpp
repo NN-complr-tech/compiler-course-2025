5cfea0219775e9f84b264f29097bd809e1ef3ef1
Kirius257 <holin.kirill@yandex.ru>
2025-03-24 19:11:30 +0300

    clang-format + add empty strings

--- llvm/compiler-course/llvm-ir/Kholin_K_DivOpt/Kholin_K_DivOpt_pass.cpp
+++ llvm/compiler-course/llvm-ir/Kholin_K_DivOpt/Kholin_K_DivOpt_pass.cpp
@@ -13,29 +13,27 @@ namespace {
 
 void visitor(Function &F) {
   errs() << "Start processing function: " << F.getName() << "\n";
-  for (auto &BB : F) {  // function cycle block//
+  for (auto &BB : F) { // function cycle block//
     errs() << ">Start processing basic block"
            << "\n";
     std::vector<BinaryOperator *> DivsToReplace;
-    for (auto &I : BB) {  // instruction cycle block
+    for (auto &I : BB) { // instruction cycle block
       if (auto *BinOp = dyn_cast<BinaryOperator>(&I)) {
         if (BinOp->getOpcode() == Instruction::SDiv ||
             BinOp->getOpcode() == Instruction::UDiv) {
           Value *Divisor = BinOp->getOperand(
-              1);  // get right operand from result = lhs op rhs
+              1); // get right operand from result = lhs op rhs
           if (ConstantInt *ConstantDivisor = dyn_cast<ConstantInt>(Divisor)) {
-            if (ConstantDivisor->getValue() == 0) {  // value of rhs
+            if (ConstantDivisor->getValue() == 0) { // value of rhs
               errs() << "Division by zero detected in function " << F.getName()
                      << "\n";
               continue;
             }
-            if (ConstantDivisor->getValue()
-                    .isPowerOf2()) {  // standart function for check degree 2
+            if (ConstantDivisor->getValue().isPowerOf2()) { // check degree 2
               errs() << "Processing div instruction: " << *BinOp << "\n";
               errs() << "Divisor is a power of 2: " << *ConstantDivisor << "\n";
               if (BinOp->getOpcode() == Instruction::SDiv &&
-                  ConstantDivisor
-                      ->isNegative()) {  // except situation when negative
+                  ConstantDivisor->isNegative()) { // negative case
                 errs() << "Signed division by negative power of 2 detected in "
                           "function "
                        << F.getName() << "\n";
@@ -48,28 +46,27 @@ void visitor(Function &F) {
       }
     }
 
-    for (auto *BinOp :
-         DivsToReplace) {  // replacing all div instructions to shift
+    for (auto *BinOp : DivsToReplace) { // replace div with shift
       uint32_t ShiftAmount =
           cast<ConstantInt>(BinOp->getOperand(1))->getValue().exactLogBase2();
       errs() << "Shift amount: " << ShiftAmount << "\n";
 
-      IRBuilder<> Builder(BinOp);  // alloca builder
+      IRBuilder<> Builder(BinOp); // initialize builder
       errs() << "Initialize Builder for instruction: " << *BinOp << "\n";
 
       Value *Shift = nullptr;
 
-      if (BinOp->getOpcode() == Instruction::SDiv) {  // for signed
+      if (BinOp->getOpcode() == Instruction::SDiv) { // signed
         Shift = Builder.CreateAShr(BinOp->getOperand(0), ShiftAmount);
-      } else {  // for unsigned
+      } else { // unsigned
         Shift = Builder.CreateLShr(BinOp->getOperand(0), ShiftAmount);
       }
       errs() << "Constructed shift instruction: " << *Shift << "\n";
 
-      BinOp->replaceAllUsesWith(Shift);  // finally replace
+      BinOp->replaceAllUsesWith(Shift); // replace
       errs() << "Division successfully replaced!\n";
 
-      BinOp->eraseFromParent();  // delete old op
+      BinOp->eraseFromParent(); // remove old
       errs() << "Div instruction successfully removed!\n";
     }
     errs() << ">End processing basic block"
@@ -81,13 +78,13 @@ void visitor(Function &F) {
 struct DivOptPass : PassInfoMixin<DivOptPass> {
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
     visitor(F);
-    return PreservedAnalyses::all();  // valid all passes
+    return PreservedAnalyses::all(); // preserve all passes
   }
 
   static bool isRequired() { return true; }
 };
 
-}  // namespace
+} // namespace
 
 llvm::PassPluginLibraryInfo getDivToShiftPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "DivOptPass", LLVM_VERSION_STRING,
@@ -95,7 +92,7 @@ llvm::PassPluginLibraryInfo getDivToShiftPluginInfo() {
             PB.registerPipelineParsingCallback(
                 [](StringRef Name, FunctionPassManager &FPM,
                    ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "div-opt-pass") {
+                  if (Name == "div-to-shift") {
                     FPM.addPass(DivOptPass());
                     return true;
                   }
