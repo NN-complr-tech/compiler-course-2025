696ae127b58c7b329a22404989b55eb8a04fc845
Ivan-Lysov <ivan.lysov.05@gmail.com>
2025-03-25 17:37:23 +0300

    fix review: add more tests, fix cpp

--- clang/compiler-course/lysov_ivan_1stLabs/lysov_ivan_1stLabs.cpp
+++ clang/compiler-course/lysov_ivan_1stLabs/lysov_ivan_1stLabs.cpp
@@ -6,37 +6,24 @@
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "llvm/Support/raw_ostream.h"
 
-using llvm::outs;
-
 namespace {
-
-class UnusedVarLabel final : public clang::RecursiveASTVisitor<UnusedVarLabel> {
+class UnusedVarMyVisitor final
+    : public clang::RecursiveASTVisitor<UnusedVarMyVisitor> {
 public:
-  explicit UnusedVarLabel(clang::ASTContext *ctx, clang::Rewriter &rwrt)
+  explicit UnusedVarMyVisitor(clang::ASTContext *ctx, clang::Rewriter &rwrt)
       : context(ctx), rewriter(rwrt) {}
-
   bool VisitVarDecl(clang::VarDecl *var) {
-    if (!var->isUsed() && !var->isImplicit() &&
-        !var->hasAttr<clang::UnusedAttr>()) {
-      outs() << "Marking variable as maybe_unused: " << var->getNameAsString()
-             << "\n";
-      auto *attr = clang::UnusedAttr::CreateImplicit(*context);
-      var->addAttr(attr);
+    if (!var->isUsed() && !var->hasAttr<clang::UnusedAttr>()) {
       clang::SourceLocation loc = var->getSourceRange().getBegin();
-      rewriter.InsertText(loc, "[[maybe_unused]] ", true, true);
+      rewriter.InsertText(loc, "[[maybe_unused]]");
     }
     return true;
   }
 
   bool VisitParmVarDecl(clang::ParmVarDecl *param) {
-    if (!param->isUsed() && !param->isImplicit() &&
-        !param->hasAttr<clang::UnusedAttr>()) {
-      outs() << "Marking parameter as maybe_unused: "
-             << param->getNameAsString() << "\n";
-      auto *attr = clang::UnusedAttr::CreateImplicit(*context);
-      param->addAttr(attr);
+    if (!param->isUsed() && !param->hasAttr<clang::UnusedAttr>()) {
       clang::SourceLocation loc = param->getSourceRange().getBegin();
-      rewriter.InsertText(loc, "[[maybe_unused]] ", true, true);
+      rewriter.InsertText(loc, "[[maybe_unused]]");
     }
     return true;
   }
@@ -46,9 +33,9 @@ private:
   clang::Rewriter &rewriter;
 };
 
-class ASTProcessor final : public clang::ASTConsumer {
+class UnusedVarConsumer final : public clang::ASTConsumer {
 public:
-  explicit ASTProcessor(clang::ASTContext *ctx, clang::Rewriter &rwrt)
+  explicit UnusedVarConsumer(clang::ASTContext *ctx, clang::Rewriter &rwrt)
       : Label(ctx, rwrt) {}
 
   void HandleTranslationUnit(clang::ASTContext &ctx) override {
@@ -56,15 +43,15 @@ public:
   }
 
 private:
-  UnusedVarLabel Label;
+  UnusedVarMyVisitor Label;
 };
 
-class PluginEntry final : public clang::PluginASTAction {
+class UnusedVarAction final : public clang::PluginASTAction {
 public:
   std::unique_ptr<clang::ASTConsumer>
   CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
     rewriter.setSourceMgr(ci.getSourceManager(), ci.getLangOpts());
-    return std::make_unique<ASTProcessor>(&ci.getASTContext(), rewriter);
+    return std::make_unique<UnusedVarConsumer>(&ci.getASTContext(), rewriter);
   }
 
   bool ParseArgs(const clang::CompilerInstance &ci,
@@ -83,7 +70,7 @@ private:
 
 } // namespace
 
-static clang::FrontendPluginRegistry::Add<PluginEntry>
+static clang::FrontendPluginRegistry::Add<UnusedVarAction>
     X("unUsedVarPlugin_LysovIvan_FIIT3_ClangAST",
       "The plugin detects unused variables and function parameters in code and "
       "adds the [[maybe_unused]] attribute.");
