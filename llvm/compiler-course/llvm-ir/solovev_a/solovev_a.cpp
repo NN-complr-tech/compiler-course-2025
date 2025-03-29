3dd6fc6a40ff6408ed78efc88d1dce3045acd6e9
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-03-29 16:39:36 +0300

    change pass

--- llvm/compiler-course/llvm-ir/solovev_a/solovev_a.cpp
+++ llvm/compiler-course/llvm-ir/solovev_a/solovev_a.cpp
@@ -16,33 +16,58 @@ public:
     bool changed = false;
     for (auto &BB : F) {
       for (auto &I : llvm::make_early_inc_range(BB)) {
-        if (auto *SDiv = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
-          if (SDiv->getOpcode() == llvm::Instruction::SDiv) {
+        if (auto *Div = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
+          if (Div->getOpcode() == llvm::Instruction::SDiv) {
             if (auto *ConstInt =
-                    llvm::dyn_cast<llvm::ConstantInt>(SDiv->getOperand(1))) {
+                    llvm::dyn_cast<llvm::ConstantInt>(Div->getOperand(1))) {
               int64_t divisor = ConstInt->getSExtValue();
-              llvm::IRBuilder<> Builder(SDiv);
+              llvm::IRBuilder<> Builder(Div);  
               if (divisor == 1 || divisor == -1) {
-                llvm::Value *NewVal = Builder.CreateAdd(
-                    SDiv->getOperand(0),
-                    llvm::ConstantInt::get(SDiv->getType(), 0), "add_zero");
-                if (divisor == -1) {
-                  NewVal = Builder.CreateNeg(NewVal, "neg_tmp");
+                llvm::Value *NewVal;
+                if (divisor == 1) { 
+                  NewVal = Builder.CreateAdd(
+                  Div->getOperand(0),
+                  llvm::ConstantInt::get(Div->getType(), 0), "add_zero");
+                } else {
+                  NewVal = Builder.CreateNeg(Div->getOperand(0), "neg_tmp");
                 }
-                SDiv->replaceAllUsesWith(NewVal);
-                SDiv->eraseFromParent();
+                Div->replaceAllUsesWith(NewVal);
+                Div->eraseFromParent();
                 changed = true;
                 continue;
-              }
+              }   
               if (divisor != 0 && (abs(divisor) & (abs(divisor) - 1)) == 0) {
                 int shiftAmount = llvm::Log2_64(abs(divisor));
                 llvm::Value *Shifted = Builder.CreateAShr(
-                    SDiv->getOperand(0), shiftAmount, "sh_tmp");
+                Div->getOperand(0), shiftAmount, "sh_tmp");
                 if (divisor < 0) {
                   Shifted = Builder.CreateNeg(Shifted, "neg_tmp");
                 }
-                SDiv->replaceAllUsesWith(Shifted);
-                SDiv->eraseFromParent();
+                Div->replaceAllUsesWith(Shifted);
+                Div->eraseFromParent();
+                changed = true;
+              }
+            }
+          }                    
+          if (Div->getOpcode() == llvm::Instruction::UDiv) {
+            if (auto *ConstInt = llvm::dyn_cast<llvm::ConstantInt>(Div->getOperand(1))) {
+              uint64_t divisor = ConstInt->getZExtValue();
+              llvm::IRBuilder<> Builder(Div);                               
+              if (divisor == 1) {
+                llvm::Value* NewVal = Builder.CreateAdd(
+                Div->getOperand(0),
+                llvm::ConstantInt::get(Div->getType(), 0), "add_zero");
+                Div->replaceAllUsesWith(NewVal);
+                Div->eraseFromParent();
+                changed = true;
+                continue;
+              }
+              if (divisor != 0 && (divisor & (divisor - 1)) == 0) {
+                int shiftAmount = llvm::Log2_64(divisor);
+                llvm::Value* Shifted = Builder.CreateLShr(
+                Div->getOperand(0), shiftAmount, "lsh_tmp");
+                Div->replaceAllUsesWith(Shifted);
+                Div->eraseFromParent();
                 changed = true;
               }
             }
@@ -59,16 +84,16 @@ public:
 
 extern "C" llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "DivToShiftPass", "v0.4",
-          [](llvm::PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
-                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
-                  if (Name == "div-to-shift") {
-                    FPM.addPass(DivToShiftPass());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
+  return { LLVM_PLUGIN_API_VERSION, "DivToShiftPass", "v1.0",
+           [](llvm::PassBuilder& PB) {
+             PB.registerPipelineParsingCallback(
+                 [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
+                    llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                   if (Name == "div-to-shift") {
+                     FPM.addPass(DivToShiftPass());
+                     return true;
+                   }
+                   return false;
+                 });
+           }};
 }
