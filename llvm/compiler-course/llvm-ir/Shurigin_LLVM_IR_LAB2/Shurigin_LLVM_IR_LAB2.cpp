0421611002419f072af4d189804a4a8c5c6fdf50
Шурыгин Сергей <Shurigin11@mail.ru>
2025-03-31 11:30:13 +0300

    fix 2/3

--- llvm/compiler-course/llvm-ir/Shurigin_LLVM_IR_LAB2/Shurigin_LLVM_IR_LAB2.cpp
+++ llvm/compiler-course/llvm-ir/Shurigin_LLVM_IR_LAB2/Shurigin_LLVM_IR_LAB2.cpp
@@ -1,3 +1,4 @@
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
@@ -6,96 +7,85 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
-using namespace llvm;
-
 namespace {
-
-static bool isPowerOfTwo(int64_t n) {
-  if (n > 0) {
-    return (n & (n - 1)) == 0;
-  } else if (n < 0) {
-    return isPowerOfTwo(-n);
-  }
-  return false;
-}
-
-static unsigned getLog2(int64_t n) {
-  n = n < 0 ? -n : n;
-  unsigned log = 0;
-  while (n >>= 1)
-    ++log;
-  return log;
-}
-
-struct DivisionToShiftPass : PassInfoMixin<DivisionToShiftPass> {
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+struct DivisionToShiftPass : llvm::PassInfoMixin<DivisionToShiftPass> {
+  llvm::PreservedAnalyses run(llvm::Function &F,
+                              llvm::FunctionAnalysisManager &) {
     bool changed = false;
 
     for (auto &BB : F) {
-      std::vector<Instruction *> toReplace;
+      llvm::SmallVector<llvm::Instruction *, 8> toReplace;
 
       for (auto &I : BB) {
-        if (auto *BO = dyn_cast<BinaryOperator>(&I)) {
-          if (BO->getOpcode() == Instruction::SDiv ||
-              BO->getOpcode() == Instruction::UDiv) {
-            if (auto *C = dyn_cast<ConstantInt>(BO->getOperand(1))) {
-              int64_t divisor = C->getSExtValue();
+        auto *BO = llvm::dyn_cast<llvm::BinaryOperator>(&I);
+        if (!BO || (BO->getOpcode() != llvm::Instruction::SDiv &&
+                    BO->getOpcode() != llvm::Instruction::UDiv)) {
+          continue;
+        }
 
-              if (divisor == 1) {
-                IRBuilder<> Builder(BO);
-                Value *Result = Builder.CreateAdd(
-                    BO->getOperand(0), ConstantInt::get(C->getType(), 0));
-                BO->replaceAllUsesWith(Result);
-                toReplace.push_back(BO);
-                changed = true;
-                continue;
-              }
+        auto *C = llvm::dyn_cast<llvm::ConstantInt>(BO->getOperand(1));
+        if (!C) {
+          continue;
+        }
 
-              if (isPowerOfTwo(std::abs(divisor))) {
-                unsigned shift = getLog2(std::abs(divisor));
-                IRBuilder<> Builder(BO);
-                Value *ShiftResult;
+        int64_t divisor = C->getSExtValue();
+        if (divisor == 1) {
+          llvm::IRBuilder<> Builder(BO);
+          llvm::Value *Result = Builder.CreateAdd(
+              BO->getOperand(0), llvm::ConstantInt::get(C->getType(), 0));
+          BO->replaceAllUsesWith(Result);
+          toReplace.push_back(BO);
+          changed = true;
+          continue;
+        }
 
-                if (BO->getOpcode() == Instruction::SDiv) {
-                  ShiftResult = Builder.CreateAShr(
-                      BO->getOperand(0), ConstantInt::get(C->getType(), shift));
-                } else {
-                  ShiftResult = Builder.CreateLShr(
-                      BO->getOperand(0), ConstantInt::get(C->getType(), shift));
-                }
+        if (llvm::isPowerOf2_64(std::abs(divisor))) {
+          unsigned shift = llvm::Log2(std::abs(divisor));
+          llvm::IRBuilder<> Builder(BO);
+          llvm::Value *ShiftResult;
 
-                if (divisor < 0) {
-                  ShiftResult = Builder.CreateSub(
-                      ConstantInt::get(C->getType(), 0), ShiftResult);
-                }
-
-                BO->replaceAllUsesWith(ShiftResult);
-                toReplace.push_back(BO);
-                changed = true;
-              }
-            }
+          if (BO->getOpcode() == llvm::Instruction::SDiv) {
+            ShiftResult = Builder.CreateAShr(
+                BO->getOperand(0), llvm::ConstantInt::get(C->getType(), shift));
+          } else {
+            ShiftResult = Builder.CreateLShr(
+                BO->getOperand(0), llvm::ConstantInt::get(C->getType(), shift));
           }
+
+          if (divisor < 0) {
+            ShiftResult = Builder.CreateSub(
+                llvm::ConstantInt::get(C->getType(), 0), ShiftResult);
+          }
+
+          BO->replaceAllUsesWith(ShiftResult);
+          toReplace.push_back(BO);
+          changed = true;
         }
       }
-
-      for (auto *I : toReplace) {
-        I->eraseFromParent();
-      }
     }
-
-    return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
+}
 
-  static bool isRequired() { return true; }
-};
+for (auto *I : toReplace) {
+  I->eraseFromParent();
+}
+}
+
+return changed ? llvm::PreservedAnalyses::none()
+               : llvm::PreservedAnalyses::all();
+}
+
+static bool isRequired() { return true; }
+}
+;
 } // namespace
 
 llvm::PassPluginLibraryInfo getDivToShiftPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "DivisionToShiftPass", "v1.0",
-          [](PassBuilder &PB) {
+          [](llvm::PassBuilder &PB) {
             PB.registerPipelineParsingCallback(
-                [](StringRef Name, FunctionPassManager &FPM,
-                   ArrayRef<PassBuilder::PipelineElement>) -> bool {
+                [](llvm::StringRef Name, llvm::FunctionPassManager &FPM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
                   if (Name == "div2shift") {
                     FPM.addPass(DivisionToShiftPass());
                     return true;
