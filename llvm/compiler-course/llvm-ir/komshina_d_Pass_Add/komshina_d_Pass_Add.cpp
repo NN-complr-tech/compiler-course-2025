f722a02969c8426c3785a363cf4f8d04ba0bd494
MikasaAkc <daria.komshina@mail.ru>
2025-04-02 14:22:21 +0300

    komshina_d_Pass_Add

--- llvm/compiler-course/llvm-ir/komshina_d_Pass_Add/komshina_d_Pass_Add.cpp
+++ llvm/compiler-course/llvm-ir/komshina_d_Pass_Add/komshina_d_Pass_Add.cpp
@@ -1,65 +1,76 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
-#include "llvm/IR/Module.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 
-using namespace llvm;
-
 namespace {
-    struct ExamplePass : PassInfoMixin<ExamplePass> {
-        PreservedAnalyses run(Function& F, FunctionAnalysisManager&) {
-            Module* M = F.getParent();
-            Function* addFunc = nullptr;
+    struct ReplaceAddSubPass : llvm::PassInfoMixin<ReplaceAddSubPass> {
+        llvm::PreservedAnalyses run(llvm::Function& F,
+            llvm::FunctionAnalysisManager&) {
+            if (F.getName() == "add" || F.getName() == "sub") {
+                return llvm::PreservedAnalyses::all();
+            }
 
-            for (Function& Func : M->functions()) {
+            llvm::Function* addFunction = nullptr;
+            llvm::Function* subFunction = nullptr;
+            llvm::Module* M = F.getParent();
+
+            for (llvm::Function& Func : M->functions()) {
                 if (Func.getName() == "add" && Func.arg_size() == 2) {
-                    addFunc = &Func;
-                    break;
+                    addFunction = &Func;
+                }
+                else if (Func.getName() == "sub" && Func.arg_size() == 2) {
+                    subFunction = &Func;
                 }
             }
 
-            if (!addFunc) {
-                return PreservedAnalyses::all();
-            }
-
             bool changed = false;
-
-            for (BasicBlock& BB : F) {
-                for (Instruction& I : make_early_inc_range(BB)) {
-                    if (auto* binOp = dyn_cast<BinaryOperator>(&I)) {
-                        if (binOp->getOpcode() == Instruction::Add) {
-                            if (addFunc->getFunctionType()->getReturnType() == binOp->getType() &&
-                                addFunc->getFunctionType()->getParamType(0) == binOp->getOperand(0)->getType() &&
-                                addFunc->getFunctionType()->getParamType(1) == binOp->getOperand(1)->getType()) {
-                                IRBuilder<> builder(binOp);
-                                Value* call = builder.CreateCall(addFunc, { binOp->getOperand(0), binOp->getOperand(1) });
-                                binOp->replaceAllUsesWith(call);
-                                binOp->eraseFromParent();
-                                changed = true;
+            for (llvm::BasicBlock& BB : F) {
+                for (llvm::Instruction& I : llvm::make_early_inc_range(BB)) {
+                    if (auto* binOp = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
+                        llvm::Function* replacementFunction = nullptr;
+                        if (binOp->getOpcode() == llvm::Instruction::Add) {
+                            if (addFunction && binOp->getOperand(0)->getType() == addFunction->getArg(0)->getType() &&
+                                binOp->getOperand(1)->getType() == addFunction->getArg(1)->getType()) {
+                                replacementFunction = addFunction;
                             }
+                        }
+                        else if (binOp->getOpcode() == llvm::Instruction::Sub) {
+                            if (subFunction && binOp->getOperand(0)->getType() == subFunction->getArg(0)->getType() &&
+                                binOp->getOperand(1)->getType() == subFunction->getArg(1)->getType()) {
+                                replacementFunction = subFunction;
+                            }
+                        }
+                        if (replacementFunction) {
+                            llvm::IRBuilder<> builder(binOp);
+                            llvm::Value* call = builder.CreateCall(
+                                replacementFunction, { binOp->getOperand(0), binOp->getOperand(1) });
+                            call->setName(binOp->getName());
+                            binOp->replaceAllUsesWith(call);
+                            binOp->eraseFromParent();
+                            changed = true;
                         }
                     }
                 }
             }
-
-            return changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
+            return changed ? llvm::PreservedAnalyses::none() : llvm::PreservedAnalyses::all();
         }
 
         static bool isRequired() { return true; }
     };
 } // namespace
 
-extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
-    return { LLVM_PLUGIN_API_VERSION, "ExamplePass", "0.1",
-            [](PassBuilder& PB) {
+extern "C" LLVM_ATTRIBUTE_WEAK llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return { LLVM_PLUGIN_API_VERSION, "ReplaceAddSubPass", "0.1",
+            [](llvm::PassBuilder& PB) {
               PB.registerPipelineParsingCallback(
-                  [](StringRef Name, FunctionPassManager& FPM,
-                     ArrayRef<PassBuilder::PipelineElement>) -> bool {
-                    if (Name == "example") {
-                      FPM.addPass(ExamplePass{});
+                  [](llvm::StringRef name, llvm::FunctionPassManager& FPM,
+                     llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
+                    if (name == "ReplaceAddSubPass") {
+                      FPM.addPass(ReplaceAddSubPass{});
                       return true;
                     }
                     return false;
