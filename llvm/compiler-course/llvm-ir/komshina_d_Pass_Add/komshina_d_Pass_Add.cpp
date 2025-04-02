bef2f4f0846b5b1a0c1f03a84ed67b3eb82d9349
MikasaAkc <daria.komshina@mail.ru>
2025-04-02 16:23:06 +0300

    komshina_d_Pass_Add

--- llvm/compiler-course/llvm-ir/komshina_d_Pass_Add/komshina_d_Pass_Add.cpp
+++ llvm/compiler-course/llvm-ir/komshina_d_Pass_Add/komshina_d_Pass_Add.cpp
@@ -15,21 +15,22 @@ namespace {
             }
 
             llvm::Module* M = F.getParent();
-
-            llvm::Function* addFunction = llvm::find_if(M->functions(), [](llvm::Function& Func) {
+            auto it = llvm::find_if(M->functions(), [](llvm::Function& Func) {
                 return Func.getName() == "add" && Func.arg_size() == 2;
                 });
 
-            if (!addFunction) {
+            if (it == M->functions().end()) {
                 return llvm::PreservedAnalyses::all();
             }
 
+            llvm::Function* addFunction = &*it;
+
             bool modified = false;
+            std::vector<llvm::Instruction*> toErase;
 
             for (llvm::BasicBlock& BB : F) {
-                for (auto it = BB.begin(), end = BB.end(); it != end;) {
-                    llvm::Instruction* I = &*it++;
-                    if (auto* binOp = llvm::dyn_cast<llvm::BinaryOperator>(I)) {
+                for (llvm::Instruction& I : BB) {
+                    if (auto* binOp = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
                         if (binOp->getOpcode() == llvm::Instruction::Add &&
                             binOp->getOperand(0)->getType() == addFunction->getArg(0)->getType() &&
                             binOp->getOperand(1)->getType() == addFunction->getArg(1)->getType()) {
@@ -38,11 +39,15 @@ namespace {
                             llvm::Value* call = builder.CreateCall(addFunction, { binOp->getOperand(0), binOp->getOperand(1) });
                             call->setName(binOp->getName());
                             binOp->replaceAllUsesWith(call);
-                            binOp->eraseFromParent();
+                            toErase.push_back(binOp);
                             modified = true;
                         }
                     }
                 }
+            }
+
+            for (llvm::Instruction* I : toErase) {
+                I->eraseFromParent();
             }
 
             return modified ? llvm::PreservedAnalyses::none() : llvm::PreservedAnalyses::all();
@@ -52,7 +57,7 @@ namespace {
     };
 } // namespace
 
-extern "C" LLVM_ATTRIBUTE_WEAK::llvm::PassPluginLibraryInfo
+extern "C" LLVM_ATTRIBUTE_WEAK llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
     return { LLVM_PLUGIN_API_VERSION, "ReplaceAddPass", "0.1",
             [](llvm::PassBuilder& PB) {
