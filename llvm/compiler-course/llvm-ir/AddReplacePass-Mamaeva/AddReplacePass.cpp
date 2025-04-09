23dadb96811c8ac3bdf94c25cbb86811d27f9a28
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-04-09 10:50:54 +0000

    Update AddReplacePass.cpp

--- llvm/compiler-course/llvm-ir/AddReplacePass-Mamaeva/AddReplacePass.cpp
+++ llvm/compiler-course/llvm-ir/AddReplacePass-Mamaeva/AddReplacePass.cpp
@@ -10,54 +10,57 @@ namespace {
 struct AddReplacePass : llvm::PassInfoMixin<AddReplacePass> {
   llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &) {
     bool Changed = false;
-
-    // Находим функцию add в модуле
-    llvm::Function *TargetFunc = M.getFunction("add");
-    if (!TargetFunc || TargetFunc->arg_size() != 2) {
+    
+    // Ранняя проверка на отсутствие функции add
+    if (M.getFunction("add") == nullptr) {
       return llvm::PreservedAnalyses::all();
     }
 
-    // Получаем типы аргументов целевой функции
+    // Находим функцию add в модуле
+    llvm::Function *TargetFunc = M.getFunction("add");
+    if (!TargetFunc || TargetFunc->arg_size() != 2 ||
+        !TargetFunc->getReturnType()->isIntegerTy() ||
+        !TargetFunc->getArg(0)->getType()->isIntegerTy() ||
+        !TargetFunc->getArg(1)->getType()->isIntegerTy()) {
+      return llvm::PreservedAnalyses::all();
+    }
+
+    // Получаем типы аргументов
     llvm::Type *ArgTy1 = TargetFunc->getArg(0)->getType();
     llvm::Type *ArgTy2 = TargetFunc->getArg(1)->getType();
+    llvm::Type *ReturnTy = TargetFunc->getReturnType();
 
     for (llvm::Function &F : M) {
-      // Пропускаем саму функцию add
-      if (&F == TargetFunc)
-        continue;
+      if (&F == TargetFunc) continue;
 
       for (llvm::BasicBlock &BB : F) {
         llvm::SmallVector<llvm::BinaryOperator *> AddInstructions;
 
-        // Собираем все инструкции add для замены
         for (llvm::Instruction &I : BB) {
           if (auto *BO = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
-            if (BO->getOpcode() == llvm::Instruction::Add) {
-              // Проверяем соответствие типов
-              if (BO->getOperand(0)->getType() == ArgTy1 &&
-                  BO->getOperand(1)->getType() == ArgTy2) {
-                AddInstructions.push_back(BO);
-              }
+            if (BO->getOpcode() == llvm::Instruction::Add &&
+                BO->getType() == ReturnTy &&
+                BO->getOperand(0)->getType() == ArgTy1 &&
+                BO->getOperand(1)->getType() == ArgTy2) {
+              AddInstructions.push_back(BO);
             }
           }
         }
 
-        // Выполняем замену
         for (auto *AddInst : AddInstructions) {
           llvm::IRBuilder<> Builder(AddInst);
-          llvm::Value *Args[] = {AddInst->getOperand(0),
-                                 AddInst->getOperand(1)};
-          llvm::CallInst *Call = Builder.CreateCall(TargetFunc, Args);
-
-          AddInst->replaceAllUsesWith(Call);
-          AddInst->eraseFromParent();
-          Changed = true;
+          llvm::Value *Args[] = {AddInst->getOperand(0), AddInst->getOperand(1)};
+          if (llvm::CallInst *Call = Builder.CreateCall(TargetFunc, Args)) {
+            AddInst->replaceAllUsesWith(Call);
+            AddInst->eraseFromParent();
+            Changed = true;
+          }
         }
       }
     }
 
     return Changed ? llvm::PreservedAnalyses::none()
-                   : llvm::PreservedAnalyses::all();
+                  : llvm::PreservedAnalyses::all();
   }
 
   static bool isRequired() { return true; }
@@ -71,7 +74,7 @@ llvmGetPassPluginInfo() {
             PB.registerPipelineParsingCallback(
                 [](llvm::StringRef Name, llvm::ModulePassManager &MPM,
                    llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
-                  if (Name == "add-replace") { // Имя для вызова пасса
+                  if (Name == "add-replace") {
                     MPM.addPass(AddReplacePass{});
                     return true;
                   }
