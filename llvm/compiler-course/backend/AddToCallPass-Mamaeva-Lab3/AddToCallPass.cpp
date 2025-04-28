6256d42100a8fc2d4f2e2a130a08b8b77818e124
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-04-28 16:39:21 +0300

    Update AddToCallPass.cpp

--- llvm/compiler-course/backend/AddToCallPass-Mamaeva-Lab3/AddToCallPass.cpp
+++ llvm/compiler-course/backend/AddToCallPass-Mamaeva-Lab3/AddToCallPass.cpp
@@ -6,114 +6,73 @@
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
-#include "llvm/CodeGen/TargetRegisterInfo.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Module.h"
 #include "llvm/Passes/PassBuilder.h"
-#include "llvm/Support/Debug.h"
 
 #define DEBUG_TYPE "add-to-call"
 
-using namespace llvm;
-
 namespace {
 
-class AddToCallPass : public MachineFunctionPass {
+class AddToCallPass : public llvm::MachineFunctionPass {
 public:
   static char ID;
+  AddToCallPass() : llvm::MachineFunctionPass(ID) {}
 
-  AddToCallPass() : MachineFunctionPass(ID) {
-    initializeAddToCallPassPass(*PassRegistry::getPassRegistry());
-  }
-
-  bool runOnMachineFunction(MachineFunction &MF) override {
-    const X86Subtarget &ST = MF.getSubtarget<X86Subtarget>();
-    const X86InstrInfo *TII = ST.getInstrInfo();
-    TRI = ST.getRegisterInfo();
+  bool runOnMachineFunction(llvm::MachineFunction &MF) override {
+    const llvm::X86Subtarget &ST = MF.getSubtarget<llvm::X86Subtarget>();
+    const llvm::X86InstrInfo *TII = ST.getInstrInfo();
     bool Changed = false;
 
-    // Get the LLVM function and module
-    Function &F = MF.getFunction();
-    Module *M = F.getParent();
-
-    // Look for 'add' function in the module
-    Function *AddFunc = M->getFunction("add");
-    if (!AddFunc || AddFunc->isDeclaration()) {
-      LLVM_DEBUG(dbgs() << "Add function not found or is declaration\n");
+    // Ищем функцию add в модуле
+    llvm::Function *AddFunc = MF.getFunction().getParent()->getFunction("add");
+    if (!AddFunc)
       return false;
-    }
 
-    // Verify function signature: i32 (i32, i32)
+    // Проверяем сигнатуру функции add (i32, i32) -> i32
     if (AddFunc->getFunctionType()->getNumParams() != 2 ||
-        !AddFunc->getReturnType()->isIntegerTy(32) ||
+        !AddFunc->getFunctionType()->getReturnType()->isIntegerTy(32) ||
         !AddFunc->getFunctionType()->getParamType(0)->isIntegerTy(32) ||
         !AddFunc->getFunctionType()->getParamType(1)->isIntegerTy(32)) {
-      LLVM_DEBUG(dbgs() << "Add function has wrong signature\n");
       return false;
     }
 
     for (auto &MBB : MF) {
-      SmallVector<MachineInstr *, 4> ToRemove;
+      llvm::SmallVector<llvm::MachineInstr *, 8> ToErase;
 
       for (auto &MI : MBB) {
-        if (MI.getOpcode() != X86::ADD32rr)
+        // Пропускаем инструкции, которые не являются ADD
+        if (MI.getOpcode() != llvm::X86::ADD32rr)
           continue;
 
-        LLVM_DEBUG(dbgs() << "Found ADD32rr instruction: "; MI.dump());
+        // Получаем операнды
+        llvm::Register Dst = MI.getOperand(0).getReg();
+        llvm::Register Src1 = MI.getOperand(1).getReg();
+        llvm::Register Src2 = MI.getOperand(2).getReg();
+        llvm::DebugLoc DL = MI.getDebugLoc();
 
-        Register DstReg = MI.getOperand(0).getReg();
-        Register Src1Reg = MI.getOperand(1).getReg();
-        Register Src2Reg = MI.getOperand(2).getReg();
-        DebugLoc DL = MI.getDebugLoc();
-
-        // Create call instruction
-        BuildMI(MBB, MI, DL, TII->get(X86::CALL64pcrel32))
+        // Создаем вызов функции add вместо инструкции ADD
+        llvm::BuildMI(MBB, MI, DL, TII->get(llvm::X86::CALL32r))
             .addGlobalAddress(AddFunc)
-            .addReg(Src1Reg, RegState::Implicit)
-            .addReg(Src2Reg, RegState::Implicit)
-            .addReg(DstReg, RegState::ImplicitDefine)
-            .addRegMask(TRI->getCallPreservedMask(MF, F.getCallingConv()));
+            .addReg(Src1, llvm::RegState::Implicit)
+            .addReg(Src2, llvm::RegState::Implicit)
+            .addReg(Dst, llvm::RegState::Implicit | llvm::RegState::Define);
 
-        ToRemove.push_back(&MI);
+        ToErase.push_back(&MI);
         Changed = true;
       }
 
-      // Remove original ADD instructions
-      for (auto *MI : ToRemove) {
+      // Удаляем заменённые инструкции
+      for (auto *MI : ToErase)
         MI->eraseFromParent();
-      }
     }
 
     return Changed;
   }
-
-  StringRef getPassName() const override { return "Replace ADD with CALL"; }
-
-  void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.setPreservesCFG();
-    MachineFunctionPass::getAnalysisUsage(AU);
-  }
-
-private:
-  const TargetRegisterInfo *TRI;
 };
 
 char AddToCallPass::ID = 0;
 
 } // end anonymous namespace
 
-extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
-llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "AddToCallPass", "v0.1",
-          [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name, MachineFunctionPassManager &MFPM,
-                   ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "add-to-call") {
-                    MFPM.addPass(AddToCallPass());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
-}
+static llvm::RegisterPass<AddToCallPass>
+    X("add-to-call", "Replaces ADD instructions with calls to add function",
+      false, false);
