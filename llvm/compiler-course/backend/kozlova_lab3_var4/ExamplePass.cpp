f404ed1573d22968e87baad50d7af2e6bdbdb080
KatyaKozlova <katyakozlova000@mail.ru>
2025-05-02 21:25:03 +0300

    second

--- llvm/compiler-course/backend/kozlova_lab3_var4/ExamplePass.cpp
+++ llvm/compiler-course/backend/kozlova_lab3_var4/ExamplePass.cpp
@@ -17,75 +17,81 @@ using namespace llvm;
 namespace {
 class MulSubPass : public MachineFunctionPass {
 public:
-    static char ID;
-    MulSubPass() : MachineFunctionPass(ID) {}
+  static char ID;
+  MulSubPass() : MachineFunctionPass(ID) {}
 
-    bool runOnMachineFunction(MachineFunction& MF) override {
-        const X86Subtarget& ST = MF.getSubtarget<X86Subtarget>();
-        const X86InstrInfo* TII = ST.getInstrInfo();
-        MachineRegisterInfo& MRI = MF.getRegInfo();
-        bool Changed = false;
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    const X86Subtarget &ST = MF.getSubtarget<X86Subtarget>();
+    const X86InstrInfo *TII = ST.getInstrInfo();
+    MachineRegisterInfo &MRI = MF.getRegInfo();
+    bool Changed = false;
 
-        for (MachineBasicBlock& MBB : MF) {
-            for (MachineBasicBlock::iterator MI = MBB.begin(), ME = MBB.end(); MI != ME;) {
-                MachineInstr& Sub = *MI++;
+    for (MachineBasicBlock &MBB : MF) {
+      for (MachineBasicBlock::iterator MI = MBB.begin(), ME = MBB.end();
+           MI != ME;) {
+        MachineInstr &Sub = *MI++;
 
-                if (!isFloatingPointSubtraction(Sub))
-                    continue;
+        if (!isFloatingPointSubtraction(Sub))
+          continue;
 
-                MachineInstr* Mul = findMultiplyInstruction(MRI, Sub);
-                if (!Mul)
-                    continue;
+        MachineInstr *Mul = findMultiplyInstruction(MRI, Sub);
+        if (!Mul)
+          continue;
 
-                fuseMultiplySubtract(MBB, MI, Sub, Mul, TII);
-                Changed = true;
-            }
-        }
-
-        return Changed;
+        fuseMultiplySubtract(MBB, MI, Sub, Mul, TII);
+        Changed = true;
+      }
     }
+
+    return Changed;
+  }
 
 private:
-    bool isFloatingPointSubtraction(const MachineInstr& MI) const {
-        unsigned Opcode = MI.getOpcode();
-        return Opcode == X86::VSUBSSrr || Opcode == X86::VSUBSDrr;
-    }
+  bool isFloatingPointSubtraction(const MachineInstr &MI) const {
+    unsigned Opcode = MI.getOpcode();
+    return Opcode == X86::VSUBSSrr || Opcode == X86::VSUBSDrr;
+  }
 
-    MachineInstr* findMultiplyInstruction(MachineRegisterInfo& MRI, const MachineInstr& Sub) const {
-        Register SubOp2 = Sub.getOperand(2).getReg();
-        MachineInstr* Mul = MRI.getUniqueVRegDef(SubOp2);
-        if (!Mul)
-            return nullptr;
+  MachineInstr *findMultiplyInstruction(MachineRegisterInfo &MRI,
+                                        const MachineInstr &Sub) const {
+    Register SubOp2 = Sub.getOperand(2).getReg();
+    MachineInstr *Mul = MRI.getUniqueVRegDef(SubOp2);
+    if (!Mul)
+      return nullptr;
 
-        unsigned MulOpcode = Mul->getOpcode();
-        return (MulOpcode == X86::VMULSSrr || MulOpcode == X86::VMULSDrr) ? Mul : nullptr;
-    }
+    unsigned MulOpcode = Mul->getOpcode();
+    return (MulOpcode == X86::VMULSSrr || MulOpcode == X86::VMULSDrr) ? Mul
+                                                                      : nullptr;
+  }
 
-    void fuseMultiplySubtract(MachineBasicBlock& MBB, MachineBasicBlock::iterator& MI, MachineInstr& Sub, MachineInstr* Mul, const X86InstrInfo* TII) {
-        Register SubDst = Sub.getOperand(0).getReg();
-        Register SubOp1 = Sub.getOperand(1).getReg();
-        Register MulOp1 = Mul->getOperand(1).getReg();
-        Register MulOp2 = Mul->getOperand(2).getReg();
-        DebugLoc DL = Sub.getDebugLoc();
+  void fuseMultiplySubtract(MachineBasicBlock &MBB,
+                            MachineBasicBlock::iterator &MI, MachineInstr &Sub,
+                            MachineInstr *Mul, const X86InstrInfo *TII) {
+    Register SubDst = Sub.getOperand(0).getReg();
+    Register SubOp1 = Sub.getOperand(1).getReg();
+    Register MulOp1 = Mul->getOperand(1).getReg();
+    Register MulOp2 = Mul->getOperand(2).getReg();
+    DebugLoc DL = Sub.getDebugLoc();
 
-        unsigned NewOpcode = (Sub.getOpcode() == X86::VSUBSSrr) ? X86::VFNMADD213SSr : X86::VFNMADD213SDr;
+    unsigned NewOpcode = (Sub.getOpcode() == X86::VSUBSSrr)
+                             ? X86::VFNMADD213SSr
+                             : X86::VFNMADD213SDr;
 
-        BuildMI(MBB, &Sub, DL, TII->get(NewOpcode), SubDst)
-            .addReg(SubOp1)
-            .addReg(MulOp1)
-            .addReg(MulOp2)
-            .addImm(0)
-            .addReg(X86::MXCSR, RegState::Implicit);
+    BuildMI(MBB, &Sub, DL, TII->get(NewOpcode), SubDst)
+        .addReg(SubOp1)
+        .addReg(MulOp1)
+        .addReg(MulOp2)
+        .addImm(0)
+        .addReg(X86::MXCSR, RegState::Implicit);
 
-        Sub.eraseFromParent();
-        Mul->eraseFromParent();
+    Sub.eraseFromParent();
+    Mul->eraseFromParent();
 
-        MI = MBB.begin();
-    }
-};
-    char MulSubPass::ID = 0;
+    MI = MBB.begin();
+  }
+ };
+char MulSubPass::ID = 0;
 } // namespace
 
 static RegisterPass<MulSubPass>
-X("mul_sub", "Fuse MUL + SUB into VFMSUB213 instructions", false,
-    false);
+    X("mul_sub", "Fuse MUL + SUB into VFMSUB213 instructions", false, false);
