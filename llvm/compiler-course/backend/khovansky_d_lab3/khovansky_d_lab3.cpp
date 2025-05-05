00e40bcc2a3d82559879c9c202f20126aa50d2a9
Kvoks <khov.dm@gmail.com>
2025-05-05 17:23:27 +0300

    rework tests and identifyFMA

--- llvm/compiler-course/backend/khovansky_d_lab3/khovansky_d_lab3.cpp
+++ llvm/compiler-course/backend/khovansky_d_lab3/khovansky_d_lab3.cpp
@@ -27,15 +27,16 @@ public:
     for (auto &Block : MF) {
       for (auto Inst = Block.begin(), End = Block.end(); Inst != End;) {
         MachineInstr &MI = *Inst++;
-        unsigned MulOp, AddOp, MulOp1, MulOp2;
+        unsigned MulOp, AddOp;
+        unsigned A_idx, B_idx, C_idx;
 
-        if (!identifyFMA(MI.getOpcode(), MulOp, AddOp, MulOp1, MulOp2))
+        if (!identifyFMA(MI.getOpcode(), MulOp, AddOp, A_idx, B_idx, C_idx))
           continue;
 
         Register Dst = MI.getOperand(0).getReg();
-        Register A = MI.getOperand(1).getReg();
-        Register B = MI.getOperand(2).getReg();
-        Register C = MI.getOperand(3).getReg();
+        Register A = MI.getOperand(A_idx).getReg();
+        Register B = MI.getOperand(B_idx).getReg();
+        Register C = MI.getOperand(C_idx).getReg();
         DebugLoc Loc = MI.getDebugLoc();
 
         const TargetRegisterClass *RegClass = MRI.getRegClass(A);
@@ -52,63 +53,99 @@ public:
   }
 
 private:
-  bool identifyFMA(unsigned Op, unsigned &Mul, unsigned &Add, unsigned &MulOp1,
-                   unsigned &MulOp2) {
+  bool identifyFMA(unsigned Op, unsigned &Mul, unsigned &Add, 
+                   unsigned &A_idx, unsigned &B_idx, unsigned &C_idx) {
     switch (Op) {
     // Packed single-precision
     case X86::VFMADD132PSr:
       Mul = X86::MULPSrr;
       Add = X86::ADDPSrr;
+      A_idx = 1;
+      B_idx = 3;
+      C_idx = 2;
       return true;
     case X86::VFMADD213PSr:
       Mul = X86::MULPSrr;
       Add = X86::ADDPSrr;
+      A_idx = 2;
+      B_idx = 1;
+      C_idx = 3;
       return true;
     case X86::VFMADD231PSr:
       Mul = X86::MULPSrr;
       Add = X86::ADDPSrr;
+      A_idx = 2;
+      B_idx = 3;
+      C_idx = 1;
       return true;
 
     // Packed double-precision
     case X86::VFMADD132PDr:
       Mul = X86::MULPDrr;
       Add = X86::ADDPDrr;
+      A_idx = 1;
+      B_idx = 3;
+      C_idx = 2;
       return true;
     case X86::VFMADD213PDr:
       Mul = X86::MULPDrr;
       Add = X86::ADDPDrr;
+      A_idx = 2;
+      B_idx = 1;
+      C_idx = 3;
       return true;
     case X86::VFMADD231PDr:
       Mul = X86::MULPDrr;
       Add = X86::ADDPDrr;
+      A_idx = 2;
+      B_idx = 3;
+      C_idx = 1;
       return true;
 
     // Scalar single-precision
     case X86::VFMADD132SSr:
       Mul = X86::MULSSrr;
       Add = X86::ADDSSrr;
+      A_idx = 1;
+      B_idx = 3;
+      C_idx = 2;
       return true;
     case X86::VFMADD213SSr:
       Mul = X86::MULSSrr;
       Add = X86::ADDSSrr;
+      A_idx = 2;
+      B_idx = 1;
+      C_idx = 3;
       return true;
     case X86::VFMADD231SSr:
       Mul = X86::MULSSrr;
       Add = X86::ADDSSrr;
+      A_idx = 2;
+      B_idx = 3;
+      C_idx = 1;
       return true;
 
     // Scalar double-precision
     case X86::VFMADD132SDr:
       Mul = X86::MULSDrr;
       Add = X86::ADDSDrr;
+      A_idx = 1;
+      B_idx = 3;
+      C_idx = 2;
       return true;
     case X86::VFMADD213SDr:
       Mul = X86::MULSDrr;
       Add = X86::ADDSDrr;
+      A_idx = 2;
+      B_idx = 1;
+      C_idx = 3;
       return true;
     case X86::VFMADD231SDr:
       Mul = X86::MULSDrr;
       Add = X86::ADDSDrr;
+      A_idx = 2;
+      B_idx = 3;
+      C_idx = 1;
       return true;
 
     default:
