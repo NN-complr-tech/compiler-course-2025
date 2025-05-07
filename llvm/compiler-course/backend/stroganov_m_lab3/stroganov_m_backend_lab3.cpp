2f51a297c7170659c4fac6912382af56b7739ab6
StroganovM <mikhail_stroganov@bk.ru>
2025-05-07 09:32:38 +0300

    add task

---
+++ llvm/compiler-course/backend/stroganov_m_lab3/stroganov_m_backend_lab3.cpp
@@ -0,0 +1,93 @@
+#include "X86.h"
+#include "X86InstrInfo.h"
+#include "X86Subtarget.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+
+using namespace llvm;
+
+namespace {
+class X86LogicOptPass : public MachineFunctionPass {
+public:
+  static char ID;
+  X86LogicOptPass() : MachineFunctionPass(ID) {}
+
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+    MachineRegisterInfo &MRI = MF.getRegInfo();
+    bool Changed = false;
+
+    const DenseMap<unsigned, unsigned> AVXOpcodeMap = {
+        {X86::PANDrr, X86::VPANDrr},   {X86::PORrr, X86::VPORrr},
+        {X86::PXORrr, X86::VPXORrr},   {X86::ANDPSrr, X86::VANDPSrr},
+        {X86::ORPSrr, X86::VORPSrr},   {X86::XORPSrr, X86::VXORPSrr},
+        {X86::PANDNrr, X86::VPANDNrr}, {X86::ANDPDrr, X86::VANDPDrr},
+        {X86::ORPDrr, X86::VORPDrr},   {X86::XORPDrr, X86::VXORPDrr},
+    };
+
+    for (MachineBasicBlock &MBB : MF) {
+      for (auto MII = MBB.begin(), MIE = MBB.end(); MII != MIE;) {
+        MachineInstr &MI = *MII;
+        unsigned Opc = MI.getOpcode();
+
+        bool Combined = false;
+
+        if (MI.getNumOperands() >= 3 && MI.getOperand(1).isReg()) {
+          Register Op1 = MI.getOperand(1).getReg();
+          MachineInstr *DefMI = MRI.getUniqueVRegDef(Op1);
+
+          if (DefMI && MRI.hasOneUse(Op1) && DefMI->getNumOperands() >= 3 &&
+              AVXOpcodeMap.count(DefMI->getOpcode()) &&
+              AVXOpcodeMap.count(Opc)) {
+
+            unsigned AVXOpc1 = AVXOpcodeMap.lookup(DefMI->getOpcode());
+            unsigned AVXOpc2 = AVXOpcodeMap.lookup(Opc);
+
+            Register TmpReg = MRI.createVirtualRegister(MRI.getRegClass(Op1));
+
+            BuildMI(MBB, MII, DefMI->getDebugLoc(), TII->get(AVXOpc1), TmpReg)
+                .addReg(DefMI->getOperand(1).getReg())
+                .addReg(DefMI->getOperand(2).getReg());
+
+            BuildMI(MBB, MII, MI.getDebugLoc(), TII->get(AVXOpc2),
+                    MI.getOperand(0).getReg())
+                .addReg(TmpReg)
+                .addReg(MI.getOperand(2).getReg());
+
+            MBB.erase(DefMI);
+            MII = MBB.erase(MII);
+            Changed = true;
+            Combined = true;
+          }
+        }
+
+        if (!Combined) {
+          if (AVXOpcodeMap.count(Opc) && MI.getNumOperands() >= 3) {
+            unsigned NewOpc = AVXOpcodeMap.lookup(Opc);
+            BuildMI(MBB, MII, MI.getDebugLoc(), TII->get(NewOpc),
+                    MI.getOperand(0).getReg())
+                .addReg(MI.getOperand(1).getReg())
+                .addReg(MI.getOperand(2).getReg());
+            MII = MBB.erase(MII);
+            Changed = true;
+          } else {
+            ++MII;
+          }
+        }
+      }
+    }
+
+    return Changed;
+  }
+};
+
+char X86LogicOptPass::ID = 0;
+} // namespace
+
+static llvm::RegisterPass<X86LogicOptPass>
+    X("x86-logic-opt", "X86 Logical Operations Optimization Pass", false,
+      false);
