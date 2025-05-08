32d033f36552dea3d14fbc63f15ceb478df8a283
ivan.lysov.05@gmail.com <ivan.lysov.05@gmail.com>
2025-05-09 00:06:08 +0300

    fix for review

--- llvm/compiler-course/backend/lysov_i_simd_counter/IvanLysovSIMDCounter.cpp
+++ llvm/compiler-course/backend/lysov_i_simd_counter/IvanLysovSIMDCounter.cpp
@@ -1,6 +1,5 @@
 #include "X86.h"
 #include "X86InstrInfo.h"
-#include "X86RegisterInfo.h"
 #include "X86Subtarget.h"
 #include "llvm/CodeGen/MachineBasicBlock.h"
 #include "llvm/CodeGen/MachineFunction.h"
@@ -28,17 +27,16 @@ public:
     Type *Int64Ty = Type::getInt64Ty(Ctx);
     Constant *Zero = ConstantInt::get(Int64Ty, 0);
 
-    GlobalVariable *CounterGV =
+    auto *GV =
         cast<GlobalVariable>(M.getOrInsertGlobal("simd_counter", Int64Ty));
-    CounterGV->setLinkage(GlobalValue::ExternalLinkage);
-    CounterGV->setInitializer(Zero);
-    CounterGV->setAlignment(MaybeAlign(8));
+    GV->setInitializer(Zero);
+    GV->setLinkage(GlobalValue::ExternalLinkage);
+    GV->setAlignment(MaybeAlign(8));
     return true;
   }
 
   bool runOnMachineFunction(MachineFunction &MF) override {
-    MachineModuleInfo &MMI = MF.getMMI();
-    const Module *Mod = MMI.getModule();
+    const Module *Mod = MF.getMMI().getModule();
     const GlobalVariable *CounterGV = Mod->getGlobalVariable("simd_counter");
     if (!CounterGV)
       return false;
@@ -46,48 +44,48 @@ public:
     const X86Subtarget &ST = MF.getSubtarget<X86Subtarget>();
     const X86InstrInfo *TII = ST.getInstrInfo();
     MachineRegisterInfo &MRI = MF.getRegInfo();
-    const X86RegisterInfo &RI = TII->getRegisterInfo();
-    const TargetRegisterClass *GPR64RC = RI.getRegClass(X86::GR64RegClassID);
 
-    bool IsChanged = false;
-    for (auto &MBB : MF) {
-      for (auto MIIt = MBB.begin(), E = MBB.end(); MIIt != E; ++MIIt) {
-        MachineInstr &MI = *MIIt;
+    bool Changed = false;
 
-        bool IsSimd = false;
+    for (MachineBasicBlock &MBB : MF) {
+      unsigned SimdCount = 0;
+
+      for (MachineInstr &MI : MBB) {
         for (const MachineOperand &MO : MI.operands()) {
           if (!MO.isReg())
             continue;
-          Register R(MO.getReg());
-
+          Register R = MO.getReg();
           if (!R.isVirtual())
             continue;
-          const TargetRegisterClass *RC = MRI.getRegClass(MO.getReg());
+          const TargetRegisterClass *RC = MRI.getRegClass(R);
           unsigned RCID = RC->getID();
           if (RCID == X86::VR128RegClassID || RCID == X86::VR256RegClassID ||
               RCID == X86::VR512RegClassID) {
-            IsSimd = true;
+            ++SimdCount;
             break;
           }
         }
-        if (!IsSimd)
-          continue;
-
-        DebugLoc DL = MI.getDebugLoc();
-        auto InsertPt = std::next(MIIt);
-
-        BuildMI(MBB, InsertPt, DL, TII->get(X86::INC64m))
-            .addReg(X86::RIP, RegState::Implicit)
-            .addImm(1)                      // scale (unused)
-            .addReg(0, RegState::Implicit)  // index
-            .addGlobalAddress(CounterGV, 0) // символ simd_counter
-            .addReg(0, RegState::Implicit); // segment
-
-        IsChanged = true;
       }
-    }
 
-    return IsChanged;
+      if (SimdCount == 0)
+        continue;
+
+      MachineBasicBlock::iterator InsertPt = MBB.getFirstTerminator();
+      DebugLoc DL;
+      if (InsertPt != MBB.end())
+        DL = InsertPt->getDebugLoc();
+
+      BuildMI(MBB, InsertPt, DL, TII->get(X86::ADD64mi32))
+          .addReg(X86::RIP, RegState::Implicit)
+          .addImm(1)
+          .addReg(0, RegState::Implicit)
+          .addGlobalAddress(CounterGV, 0)
+          .addReg(0, RegState::Implicit)
+          .addImm(SimdCount);
+
+      Changed = true;
+    }
+    return Changed;
   }
 };
 
