ccacd3324b0211ee27c64e99634bdd14ab4cacb8
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2025-05-08 15:11:08 +0300

    fix review

--- llvm/compiler-course/backend/kolodkin_g_lab_1_var_1/kolodkin_g_lab_1_var_1_pass.cpp
+++ llvm/compiler-course/backend/kolodkin_g_lab_1_var_1/kolodkin_g_lab_1_var_1_pass.cpp
@@ -13,93 +13,118 @@
 namespace {
 class LogicalChainOptimization : public llvm::MachineFunctionPass {
 public:
-  static char ID;
-  LogicalChainOptimization() : MachineFunctionPass(ID) {}
-  const llvm::TargetInstrInfo *TII;
+    static char ID;
+    LogicalChainOptimization() : MachineFunctionPass(ID) {}
+    const llvm::TargetInstrInfo *TII;
 
-  bool runOnMachineFunction(llvm::MachineFunction &MF) override {
-    bool modified = false;
-    TII = MF.getSubtarget<llvm::X86Subtarget>().getInstrInfo();
+    bool runOnMachineFunction(llvm::MachineFunction &MF) override {
+        bool modified = false;
+        TII = MF.getSubtarget<llvm::X86Subtarget>().getInstrInfo();
 
-    for (auto &MBB : MF) {
-      std::vector<llvm::MachineInstr *> toErase;
-      for (auto I = MBB.begin(), E = MBB.end(); I != E;) {
-        llvm::MachineInstr *inst1 = &*I++;
+        for (auto &MBB : MF) {
+            std::vector<llvm::MachineInstr *> toErase;
+            for (auto I = MBB.begin(), E = MBB.end(); I != E; ) {
+                llvm::MachineInstr *inst = &*I++;
 
-        if (inst1->getOpcode() == llvm::X86::PANDrr) {
-          if (I != E) {
-            llvm::MachineInstr *inst2 = &*I;
-            if (inst2->getOpcode() == llvm::X86::PANDrr) {
-              if (inst1->getOperand(0).getReg() ==
-                  inst2->getOperand(1).getReg()) {
-                BuildMI(MBB, inst2, inst2->getDebugLoc(),
-                        TII->get(llvm::X86::PANDrr),
-                        inst1->getOperand(0).getReg())
-                    .addReg(inst2->getOperand(2).getReg())
-                    .addReg(inst1->getOperand(2).getReg());
+                if (inst->getOpcode() == llvm::X86::PORrr) {
+                    if (std::distance(I, E) < 2) {
+                        continue;
+                    }
 
-                BuildMI(MBB, inst2, inst2->getDebugLoc(),
-                        TII->get(llvm::X86::PANDrr),
-                        inst2->getOperand(0).getReg())
-                    .addReg(inst1->getOperand(0).getReg())
-                    .addReg(inst1->getOperand(1).getReg());
+                    llvm::MachineInstr *nextInst1 = &*I;
+                    llvm::MachineInstr *nextInst2 = &*(std::next(I));
 
-                toErase.push_back(inst1);
-                toErase.push_back(inst2);
 
-                modified = true;
-              }
+                    if (nextInst1->getOpcode() == llvm::X86::PORrr &&
+                        nextInst2->getOpcode() == llvm::X86::PANDrr) {
+
+                        llvm::Register reg1_0 = inst->getOperand(0).getReg();
+                        llvm::Register reg1_1 = inst->getOperand(1).getReg();
+                        llvm::Register reg1_2 = inst->getOperand(2).getReg();
+                        llvm::Register reg2_0 = nextInst1->getOperand(0).getReg();
+                        llvm::Register reg2_1 = nextInst1->getOperand(1).getReg();
+                        llvm::Register reg2_2 = nextInst1->getOperand(2).getReg();
+                        llvm::Register reg3_0 = nextInst2->getOperand(0).getReg();
+                        llvm::Register reg3_1 = nextInst2->getOperand(1).getReg();
+                        llvm::Register reg3_2 = nextInst2->getOperand(2).getReg();
+
+                        if ((reg1_1 == reg2_1 || reg1_2 == reg2_2) && (reg3_1 == reg1_0 && reg3_2 == reg2_0)) {
+                            llvm::Register resReg1, resReg2, resReg3;
+                            if (reg1_1 == reg2_1){
+                                resReg1 = reg1_2;
+                                resReg2 = reg2_2;
+                                resReg3 = reg1_1;
+                            } else if (reg1_2 == reg2_2) {
+                                resReg1 = reg1_1;
+                                resReg2 = reg2_1;
+                                resReg3 = reg1_2;
+                            }
+                            BuildMI(MBB, inst, inst->getDebugLoc(),
+                                    TII->get(llvm::X86::PANDrr), reg2_0)
+                                .addReg(resReg1)
+                                .addReg(resReg2);
+
+                            BuildMI(MBB, inst, inst->getDebugLoc(),
+                                    TII->get(llvm::X86::PORrr), reg3_0)
+                                .addReg(resReg3)
+                                .addReg(reg2_0);
+
+
+                            toErase.push_back(inst);
+                            toErase.push_back(nextInst1);
+                            toErase.push_back(nextInst2);
+
+                            modified = true;
+                        }
+                    }
+                }
             }
-          }
+            for (auto *instr : toErase) {
+                instr->eraseFromParent();
+            }
         }
-      }
-      for (auto *instr : toErase) {
-        instr->eraseFromParent();
-      }
-    }
 
-    for (auto &MBB : MF) {
-      for (auto I = MBB.begin(), E = MBB.end(); I != E;) {
-        llvm::MachineInstr *inst = &*I++;
+        for (auto &MBB : MF) {
+            for (auto I = MBB.begin(), E = MBB.end(); I != E; ) {
+                llvm::MachineInstr *inst = &*I++;
 
         switch (inst->getOpcode()) {
-        case llvm::X86::PORrr:
-          replaceInstruction(MF, MBB, inst, llvm::X86::VPORrr);
-          modified = true;
-          break;
+            case llvm::X86::PORrr:
+                replaceInstruction(MF, MBB, inst, llvm::X86::VPORrr);
+                modified = true;
+                break;
 
-        case llvm::X86::PXORrr:
-          replaceInstruction(MF, MBB, inst, llvm::X86::VPXORrr);
-          modified = true;
-          break;
+            case llvm::X86::PXORrr:
+                replaceInstruction(MF, MBB, inst, llvm::X86::VPXORrr);
+                modified = true;
+                break;
 
-        case llvm::X86::PANDrr:
-          replaceInstruction(MF, MBB, inst, llvm::X86::VPANDrr);
-          modified = true;
-          break;
+            case llvm::X86::PANDrr:
+                replaceInstruction(MF, MBB, inst, llvm::X86::VPANDrr);
+                modified = true;
+                break;
 
-        default:
-          break;
+            default:
+                break;
         }
-      }
+            }
+        }
+
+        return modified;
     }
-    return modified;
-  }
-  void replaceInstruction(llvm::MachineFunction &MF,
-                          llvm::MachineBasicBlock &MBB,
-                          llvm::MachineInstr *inst, unsigned newOpcode) {
+void replaceInstruction(llvm::MachineFunction &MF, llvm::MachineBasicBlock &MBB, llvm::MachineInstr *inst, unsigned newOpcode) {
     auto &TII = *MF.getSubtarget<llvm::X86Subtarget>().getInstrInfo();
-    BuildMI(MBB, inst, inst->getDebugLoc(), TII.get(newOpcode),
-            inst->getOperand(0).getReg())
+    BuildMI(MBB, inst, inst->getDebugLoc(), 
+            TII.get(newOpcode), inst->getOperand(0).getReg())
         .addReg(inst->getOperand(1).getReg())
         .addReg(inst->getOperand(2).getReg());
 
     inst->eraseFromParent();
-  }
+}
 };
-
 char LogicalChainOptimization::ID = 0;
 } // namespace
 
-static llvm::RegisterPass<LogicalChainOptimization>
-    X("logical-opt-x86", "Logical Optimization Pass", false, false);
+static llvm::RegisterPass<LogicalChainOptimization> X("logical-opt-x86",
+                                               "Logical Optimization Pass",
+                                               false, false);
