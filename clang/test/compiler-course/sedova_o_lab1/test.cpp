e71f664daa26fb5f736fa0cd26a4b71517fc9a08
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-05-13 22:00:35 +0300

    test

--- clang/test/compiler-course/sedova_o_lab1/test.cpp
+++ clang/test/compiler-course/sedova_o_lab1/test.cpp
@@ -1,71 +1,36 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/Lab1_Sedova_Olga_FIIT1_ClangAST%pluginext -plugin Lab1 -fsyntax-only %s 2>&1 | FileCheck %s
 
-// CHECK-LABEL: FunctionDecl {{.*}} isPositive 'bool (int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: BinaryOperator {{.*}} 'bool' '>'
+// CHECK-LABEL: Function 'isPositive' contains 0 implicit casts.
+
+// CHECK-LABEL: Function 'compute' contains 2 implicit casts.
+
+// CHECK-LABEL: Function 'process' contains 3 implicit casts.
+
+// CHECK-LABEL: Function 'identity' contains 0 implicit casts.
+
+// CHECK-LABEL: Function 'explicitCast' contains 1 implicit casts.
+
+// CHECK-LABEL: Function 'returnFloat' contains 0 implicit casts.
 
 bool isPositive(int a) {
   return a > 0;
 }
 
-// CHECK-LABEL: FunctionDecl {{.*}} compute 'double (int, float)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
-// CHECK: BinaryOperator {{.*}} 'float' '+'
-// CHECK: ImplicitCastExpr {{.*}} 'float' <IntegralToFloating>
-// CHECK: 'int' lvalue ParmVar
-// CHECK: ImplicitCastExpr {{.*}} 'float' <LValueToRValue>
-// CHECK: 'float' lvalue ParmVar
-
 double compute(int x, float y) {
   return x + y;
 }
-
-// CHECK-LABEL: FunctionDecl {{.*}} process 'int (float, float)'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'int' <FloatingToIntegral>
-// CHECK: BinaryOperator {{.*}} 'double' '+'
-// CHECK: ImplicitCastExpr {{.*}} 'double' <FloatingCast>
-// CHECK: CallExpr
-// CHECK: ImplicitCastExpr {{.*}} <FunctionToPointerDecay>
-// CHECK: 'double (int, float)' lvalue Function
-// CHECK: ImplicitCastExpr {{.*}} 'int' <FloatingToIntegral>
-// CHECK: 'float' lvalue ParmVar
 
 int process(float a, float b) {
   return a + compute(a, b);
 }
 
-// CHECK-LABEL: FunctionDecl {{.*}} identity 'int (int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'int' <LValueToRValue>
-// CHECK: 'int' lvalue ParmVar
-
 int identity(int v) {
   return v;
 }
 
-// CHECK-LABEL: FunctionDecl {{.*}} explicitCast 'float (int)'
-// CHECK: ParmVarDecl {{.*}} 'int'
-// CHECK: ReturnStmt
-// CHECK: CStyleCastExpr {{.*}} 'float'
-// CHECK: ImplicitCastExpr {{.*}} 'float' <IntegralToFloating>
-// CHECK: 'int' lvalue ParmVar
-
 float explicitCast(int a) {
   return (float)a;
 }
-
-// CHECK-LABEL: FunctionDecl {{.*}} returnFloat 'float (float)'
-// CHECK: ParmVarDecl {{.*}} 'float'
-// CHECK: ReturnStmt
-// CHECK: ImplicitCastExpr {{.*}} 'float' <LValueToRValue>
-// CHECK: 'float' lvalue ParmVar
 
 float returnFloat(float f) {
   return f;
