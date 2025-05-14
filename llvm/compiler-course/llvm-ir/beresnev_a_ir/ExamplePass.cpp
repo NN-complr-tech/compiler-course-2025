7b4fc712447f768082fb4f71241f4b778f703543
nton <antonberesnev55057@gmail.com>
2025-05-15 02:04:11 +0300

    first

---
+++ llvm/compiler-course/llvm-ir/beresnev_a_ir/ExamplePass.cpp
@@ -0,0 +1,54 @@
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace {
+struct PureFunctionPass : llvm::PassInfoMixin<PureFunctionPass> {
+
+  bool isPureFunction(llvm::Function &F) {
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto *LI = llvm::dyn_cast<llvm::LoadInst>(&I)) {
+          llvm::Value *ptr = LI->getPointerOperand();
+          if (auto *GV = llvm::dyn_cast<llvm::GlobalVariable>(
+                  ptr->stripPointerCasts()))
+            return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  llvm::PreservedAnalyses run(llvm::Function &F,
+                              llvm::FunctionAnalysisManager &) {
+    if (isPureFunction(F)) {
+      F.addFnAttr("pure");
+      llvm::outs() << "Function " << F.getName() << " marked as pure\n";
+    } else {
+      llvm::outs() << "Function " << F.getName() << " NOT pure\n";
+    }
+    return llvm::PreservedAnalyses::all();
+  }
+
+  static bool isRequired() { return true; }
+};
+} // namespace
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "PureFunctionPass", "0.1",
+          [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef name, llvm::FunctionPassManager &FPM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
+                  if (name == "pure_fn") {
+                    FPM.addPass(PureFunctionPass{});
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
---
+++ llvm/test/compiler-course/beresnev_a/test_llvm_ir.ll
@@ -0,0 +1,29 @@
+; RUN: opt -load-pass-plugin %llvmshlibdir/UserPass_Beresnev_Anton_FIIT1_LLVM_IR%pluginext\
+; RUN: -passes=pure_fn -S %s | FileCheck %s
+
+; CHECK: Function _Z3addii marked as pure
+; CHECK: Function _Z3foof NOT pure
+
+@global_value = dso_local global float 0.000000e+00, align 4
+
+define dso_local noundef i32 @_Z3addii(i32 noundef %a, i32 noundef %b) {
+entry:
+  %a.addr = alloca i32, align 4
+  %b.addr = alloca i32, align 4
+  store i32 %a, ptr %a.addr, align 4
+  store i32 %b, ptr %b.addr, align 4
+  %0 = load i32, ptr %a.addr, align 4
+  %1 = load i32, ptr %b.addr, align 4
+  %add = add nsw i32 %0, %1
+  ret i32 %add
+}
+
+define dso_local noundef float @_Z3foof(float noundef %a) {
+entry:
+  %a.addr = alloca float, align 4
+  store float %a, ptr %a.addr, align 4
+  %0 = load float, ptr %a.addr, align 4
+  %1 = load float, ptr @global_value, align 4
+  %mul = fmul float %0, %1
+  ret float %mul
+}
