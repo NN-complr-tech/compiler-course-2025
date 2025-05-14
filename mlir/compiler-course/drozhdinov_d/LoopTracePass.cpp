df137ba3852534d951fed213f9d1a12a4e60adce
Vasilina <dima-dimka04@yandex.ru>
2025-05-15 00:24:55 +0300

    improve structure and tests

--- mlir/compiler-course/drozhdinov_d/LoopTracePass.cpp
+++ mlir/compiler-course/drozhdinov_d/LoopTracePass.cpp
@@ -1,67 +1,67 @@
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/Operation.h"
-#include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
 
 namespace {
-class LoopTracePass
-    : public mlir::PassWrapper<LoopTracePass, mlir::OperationPass<mlir::ModuleOp>> {
+class LoopTracePass : public mlir::PassWrapper<LoopTracePass, mlir::OperationPass<mlir::ModuleOp>> {
 public:
-  mlir::StringRef getArgument() const final { return "LoopTracePass"; }
+  mlir::StringRef getArgument() const final override { return "LoopTracePass"; }
 
-  mlir::StringRef getDescription() const final {
-    return "Insert function calls in begin and end of the cycles";
+  mlir::StringRef getDescription() const final override {
+    return "Insert @trace_loop_iter_begin and @trace_loop_iter_end function calls in the start and the end of loops";
   }
 
   void runOnOperation() override {
     mlir::ModuleOp module = getOperation();
     mlir::OpBuilder builder(module.getContext());
 
-    auto declareTraceFunc = [&](mlir::StringRef name) {
-      if (!module.lookupSymbol<mlir::func::FuncOp>(name)) {
-        builder.setInsertionPointToStart(module.getBody());
-        auto func = builder.create<mlir::func::FuncOp>(
-            module.getLoc(), name, builder.getFunctionType({}, {}));
-        func.setSymVisibility("private");
-      }
-    };
-
-    declareTraceFunc("trace_loop_iter_begin");
-    declareTraceFunc("trace_loop_iter_end");
+    Declarator("trace_loop_iter_begin", module, builder);
+    Declarator("trace_loop_iter_end", module, builder);
 
     module.walk([&](mlir::Operation *op) {
-      if (llvm::isa<mlir::scf::ForOp, mlir::scf::WhileOp,
-                    mlir::scf::ParallelOp, mlir::affine::AffineForOp>(op)) {
-        mlir::Location loc = op->getLoc();
-        handleLoopOp(op, loc, builder);
+      if (!isRecognizedLoop(op))
+        return;
+
+      for (mlir::Region &region : op->getRegions()) {
+        if (region.empty())
+          continue;
+
+        mlir::Block &block = region.front();
+
+        builder.setInsertionPointToStart(&block);
+        builder.create<mlir::func::CallOp>(
+            op->getLoc(), "trace_loop_iter_begin",
+            mlir::TypeRange{}, mlir::ValueRange{});
+
+        builder.setInsertionPoint(block.getTerminator());
+        builder.create<mlir::func::CallOp>(
+            op->getLoc(), "trace_loop_iter_end",
+            mlir::TypeRange{}, mlir::ValueRange{});
       }
     });
   }
- private:
-  static void createcalls(mlir::Block &block, mlir::Location loc,
-                                        mlir::OpBuilder &builder) {
-    builder.setInsertionPointToStart(&block);
-    builder.create<mlir::func::CallOp>(loc, "trace_loop_iter_begin", mlir::TypeRange{}, mlir::ValueRange{});
 
-    mlir::Operation *term = block.getTerminator();
-    builder.setInsertionPoint(term);
-    builder.create<mlir::func::CallOp>(loc, "trace_loop_iter_end", mlir::TypeRange{}, mlir::ValueRange{});
+private:
+  bool isRecognizedLoop(mlir::Operation *op) {
+    return llvm::isa<mlir::scf::ForOp,
+                     mlir::scf::WhileOp,
+                     mlir::scf::ParallelOp,
+                     mlir::affine::AffineForOp>(op);
   }
 
-  static void handleLoopOp(mlir::Operation *op, mlir::Location loc, mlir::OpBuilder &builder) {
-    if (auto affineFor = mlir::dyn_cast<mlir::affine::AffineForOp>(op)) {
-      createcalls(*affineFor.getBody(), loc, builder);
-    } else if (auto scfFor = mlir::dyn_cast<mlir::scf::ForOp>(op)) {
-      createcalls(*scfFor.getBody(), loc, builder);
-    } else if (auto scfWhile = mlir::dyn_cast<mlir::scf::WhileOp>(op)) {
-      createcalls(scfWhile.getAfter().front(), loc, builder);
+  void Declarator(mlir::StringRef name, mlir::ModuleOp module, mlir::OpBuilder &builder) {
+    if (module.lookupSymbol<mlir::func::FuncOp>(name)) {
+      return;
     }
+    builder.setInsertionPointToStart(module.getBody());
+    auto func = builder.create<mlir::func::FuncOp>(
+        module.getLoc(), name, builder.getFunctionType({}, {}));
+    func.setSymVisibility("private");
   }
 };
 } // namespace
@@ -70,8 +70,12 @@ MLIR_DECLARE_EXPLICIT_TYPE_ID(LoopTracePass)
 MLIR_DEFINE_EXPLICIT_TYPE_ID(LoopTracePass)
 
 mlir::PassPluginLibraryInfo getTraceLoopPassPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "LoopTracePass", "1.0",
-          []() { mlir::PassRegistration<LoopTracePass>(); }};
+  return {
+    MLIR_PLUGIN_API_VERSION,
+    "LoopTracePass",
+    "1.0",
+    []() { mlir::PassRegistration<LoopTracePass>(); }
+  };
 }
 
 extern "C" LLVM_ATTRIBUTE_WEAK mlir::PassPluginLibraryInfo
--- mlir/test/compiler-course/drozhdinov_d/test.mlir
+++ mlir/test/compiler-course/drozhdinov_d/test.mlir
@@ -6,9 +6,10 @@ module {
   // CHECK-NEXT: func.func private @trace_loop_iter_begin()
 
   // CHECK-LABEL: func.func @affine_for()
-  // CHECK: func.call @trace_loop_iter_begin() : () -> ()
-  // CHECK-NEXT: %1 = arith.index_cast %arg0 : index to i32
-  // CHECK-NEXT: %2 = arith.addi %arg1, %1 : i32
+  // CHECK: affine.for
+  // CHECK-NEXT: func.call @trace_loop_iter_begin() : () -> ()
+  // CHECK-NEXT: %1 = arith.index_cast
+  // CHECK-NEXT: %2 = arith.addi
   // CHECK-NEXT: func.call @trace_loop_iter_end() : () -> ()
   func.func @affine_for() -> i32 {
     %sum_init = arith.constant 0 : i32
@@ -19,23 +20,59 @@ module {
     }
     return %result : i32
   }
-  
-  /// CHECK-LABEL: func.func @scf_while(%arg0: index)
+
+  // CHECK-LABEL: func.func @scf_while(
+  // CHECK: func.call @trace_loop_iter_begin() : () -> ()
+  // CHECK-NEXT: %c10 = arith.constant 10 : index
+  // CHECK-NEXT: %1 = arith.cmpi slt, %arg1, %c10 : index
+  // CHECK-NEXT: func.call @trace_loop_iter_end() : () -> ()
+  // CHECK-NEXT: scf.condition(%1) %arg1 : index
+  // CHECK-NEXT: } do {
+  // CHECK-NEXT: ^bb0(%arg1: index):
+  // CHECK-NEXT: func.call @trace_loop_iter_begin() : () -> ()
+  // CHECK-NEXT: %1 = arith.addi %arg1, %c1 : index
+  // CHECK-NEXT: func.call @trace_loop_iter_end() : () -> ()
   func.func @scf_while(%arg0: index) {
-    // CHECK:   func.call @trace_loop_iter_begin() : () -> ()
-    // CHECK:   func.call @trace_loop_iter_end() : () -> ()
-    %one   = arith.constant 1 : index
+    %one = arith.constant 1 : index
     %res = scf.while (%i = %arg0) : (index) -> (index) {
       %limit = arith.constant 10 : index
-      %cond  = arith.cmpi slt, %i, %limit : index
+      %cond = arith.cmpi slt, %i, %limit : index
       scf.condition(%cond) %i : index
     } do {
-      ^bb0(%i_curr: index):
-        %v   = arith.constant 5 : i32 
-        %inc = arith.addi %i_curr, %one : index
-        scf.yield %inc : index 
+    ^bb0(%i_curr: index):
+      %inc = arith.addi %i_curr, %one : index
+      scf.yield %inc : index
     }
     return
   }
-  
+
+  // CHECK-LABEL: func.func @scf_for()
+  // CHECK: scf.for
+  // CHECK-NEXT: func.call @trace_loop_iter_begin() : () -> ()
+  // CHECK-NEXT: %0 = arith.addi
+  // CHECK-NEXT: func.call @trace_loop_iter_end() : () -> ()
+  func.func @scf_for() {
+    %c0 = arith.constant 0 : index
+    %c10 = arith.constant 10 : index
+    %c1 = arith.constant 1 : index
+    scf.for %i = %c0 to %c10 step %c1 {
+      %x = arith.addi %i, %c1 : index
+    }
+    return
+  }
+
+  // CHECK-LABEL: func.func @scf_parallel()
+  // CHECK: scf.parallel
+  // CHECK-NEXT: func.call @trace_loop_iter_begin() : () -> ()
+  // CHECK-NEXT: %0 = arith.addi
+  // CHECK-NEXT: func.call @trace_loop_iter_end() : () -> ()
+  func.func @scf_parallel() {
+    %c0 = arith.constant 0 : index
+    %c10 = arith.constant 10 : index
+    %c1 = arith.constant 1 : index
+    scf.parallel (%i) = (%c0) to (%c10) step (%c1) {
+      %x = arith.addi %i, %c1 : index
+    }
+    return
+  }
 }
