c2fdeef892f0d3c3f98fa0479b93db2b180607c4
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2025-05-14 20:47:22 +0300

    fix clang-format

--- mlir/compiler-course/rezantseva_mlir/rezantseva_mlir.cpp
+++ mlir/compiler-course/rezantseva_mlir/rezantseva_mlir.cpp
@@ -6,37 +6,40 @@
 #include "mlir/IR/Visitors.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace mlir;
 
 namespace {
 
-class MaxDepthPass : public PassWrapper<MaxDepthPass, OperationPass<func::FuncOp>> {
+class MaxDepthPass
+    : public PassWrapper<MaxDepthPass, OperationPass<func::FuncOp>> {
 private:
-
-  bool isLoopOp(Operation* op) {
-    return isa<affine::AffineForOp, scf::ForOp, scf::WhileOp, scf::ForallOp, spirv::LoopOp>(op);
-  }
- 
-  bool isNestingOp(Operation* op) {
+  bool isLoopOp(Operation *op) {
     return isa<affine::AffineForOp, scf::ForOp, scf::WhileOp, scf::ForallOp,
-               spirv::LoopOp, affine::AffineIfOp, scf::IfOp, spirv::SelectionOp>(op);
+               spirv::LoopOp>(op);
   }
 
+  bool isNestingOp(Operation *op) {
+    return isa<affine::AffineForOp, scf::ForOp, scf::WhileOp, scf::ForallOp,
+               spirv::LoopOp, affine::AffineIfOp, scf::IfOp,
+               spirv::SelectionOp>(op);
+  }
 
-  unsigned computeDepth(Operation* op, unsigned currentDepth = 0) {
+  unsigned computeDepth(Operation *op, unsigned currentDepth = 0) {
     unsigned maxDepth = currentDepth;
 
     if (isNestingOp(op)) {
       maxDepth = std::max(maxDepth, currentDepth + 1);
     }
 
-    for (Region& region : op->getRegions()) {
-      for (Block& block : region) {
-        for (Operation& nestedOp : block) {
-          maxDepth = std::max(maxDepth, computeDepth(&nestedOp, currentDepth + (isNestingOp(op) ? 1 : 0)));
+    for (Region &region : op->getRegions()) {
+      for (Block &block : region) {
+        for (Operation &nestedOp : block) {
+          maxDepth = std::max(
+              maxDepth, computeDepth(&nestedOp,
+                                     currentDepth + (isNestingOp(op) ? 1 : 0)));
         }
       }
     }
@@ -49,44 +52,44 @@ public:
     return "MaxDepthPass_RezantsevaAnastasia_FIIT1_MLIR";
   }
 
-  StringRef getDescription() const final {
-    return "Pass ";
-  }
+  StringRef getDescription() const final { return "Pass "; }
 
   void runOnOperation() override {
     func::FuncOp func = getOperation();
-  std::vector<unsigned> loopDepths;
+    std::vector<unsigned> loopDepths;
 
-  func.walk([&](Operation* op) {
-    if (isLoopOp(op)) {
-      Operation* parent = op->getParentOp();
-      bool isNestedInLoop = false;
-      while (parent && !isa<func::FuncOp>(parent)) {
-        if (isLoopOp(parent)) {
-          isNestedInLoop = true;
-          break;
+    func.walk([&](Operation *op) {
+      if (isLoopOp(op)) {
+        Operation *parent = op->getParentOp();
+        bool isNestedInLoop = false;
+        while (parent && !isa<func::FuncOp>(parent)) {
+          if (isLoopOp(parent)) {
+            isNestedInLoop = true;
+            break;
+          }
+          parent = parent->getParentOp();
         }
-        parent = parent->getParentOp();
-      }
-      if (!isNestedInLoop) {
-        unsigned depth = computeDepth(op);
-        if (depth > 0) {
-          loopDepths.push_back(depth);
+        if (!isNestedInLoop) {
+          unsigned depth = computeDepth(op);
+          if (depth > 0) {
+            loopDepths.push_back(depth);
+          }
         }
       }
-    }
-  });
+    });
 
-  if (loopDepths.empty()) {
-    func->setAttr("my_loop_depths", ArrayAttr::get(func.getContext(), {}));
-  } else {
-    std::sort(loopDepths.begin(), loopDepths.end(), std::greater<unsigned>());
-    SmallVector<Attribute> depthAttrs;
-    for (unsigned depth : loopDepths) {
-      depthAttrs.push_back(IntegerAttr::get(IntegerType::get(func.getContext(), 64), depth));
+    if (loopDepths.empty()) {
+      func->setAttr("my_loop_depths", ArrayAttr::get(func.getContext(), {}));
+    } else {
+      std::sort(loopDepths.begin(), loopDepths.end(), std::greater<unsigned>());
+      SmallVector<Attribute> depthAttrs;
+      for (unsigned depth : loopDepths) {
+        depthAttrs.push_back(
+            IntegerAttr::get(IntegerType::get(func.getContext(), 64), depth));
+      }
+      func->setAttr("my_loop_depths",
+                    ArrayAttr::get(func.getContext(), depthAttrs));
     }
-    func->setAttr("my_loop_depths", ArrayAttr::get(func.getContext(), depthAttrs));
-  }
   }
 };
 
