ceb21b56173510e861951b9247fbead368361d7e
OlgaKotleta <90023764+OlgaKotleta@users.noreply.github.com>
2025-05-19 11:39:13 +0000

    Update rem_pass.cpp

--- mlir/compiler-course/rem_pass.cpp
+++ mlir/compiler-course/rem_pass.cpp
@@ -1,5 +1,4 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
@@ -9,66 +8,47 @@ using namespace mlir;
 
 namespace {
 
-class RemLoweringPattern : public OpRewritePattern<arith::RemSIOp> {
-public:
-  RemLoweringPattern(MLIRContext *context)
-      : OpRewritePattern<arith::RemSIOp>(context) {}
+class MamaevaRemPass
+    : public PassWrapper<MamaevaRemPass, OperationPass<ModuleOp>> {
+private:
+  void expandRemainder(Operation *op, OpBuilder &builder, bool isSigned) {
+    Value lhs = op->getOperand(0);
+    Value rhs = op->getOperand(1);
+    Location loc = op->getLoc();
 
-  LogicalResult matchAndRewrite(arith::RemSIOp op,
-                                PatternRewriter &rewriter) const override {
-    Value lhs = op.getLhs();
-    Value rhs = op.getRhs();
-    Location loc = op.getLoc();
+    Value div;
+    if (isSigned) {
+      div = builder.create<arith::DivSIOp>(loc, lhs, rhs).getResult();
+    } else {
+      div = builder.create<arith::DivUIOp>(loc, lhs, rhs).getResult();
+    }
 
-    Value div = rewriter.create<arith::DivSIOp>(loc, lhs, rhs);
-    Value mul = rewriter.create<arith::MulIOp>(loc, div, rhs);
-    Value sub = rewriter.create<arith::SubIOp>(loc, lhs, mul);
+    Value mul = builder.create<arith::MulIOp>(loc, div, rhs).getResult();
+    Value result = builder.create<arith::SubIOp>(loc, lhs, mul).getResult();
 
-    rewriter.replaceOp(op, sub);
-    return success();
+    op->getResult(0).replaceAllUsesWith(result);
+    op->erase();
   }
-};
 
-class URemLoweringPattern : public OpRewritePattern<arith::RemUIOp> {
 public:
-  URemLoweringPattern(MLIRContext *context)
-      : OpRewritePattern<arith::RemUIOp>(context) {}
-
-  LogicalResult matchAndRewrite(arith::RemUIOp op,
-                                PatternRewriter &rewriter) const override {
-    Value lhs = op.getLhs();
-    Value rhs = op.getRhs();
-    Location loc = op.getLoc();
-
-    Value div = rewriter.create<arith::DivUIOp>(loc, lhs, rhs);
-    Value mul = rewriter.create<arith::MulIOp>(loc, div, rhs);
-    Value sub = rewriter.create<arith::SubIOp>(loc, lhs, mul);
-
-    rewriter.replaceOp(op, sub);
-    return success();
-  }
-};
-
-class MamaevaRemPass : public PassWrapper<MamaevaRemPass, OperationPass<>> {
-public:
-  StringRef getArgument() const final {
-    return "rem_pass_Mamaeva_Olga_FIIT3_MLIR";
-  }
+  // Имя pass, которое должно совпадать с тестовым файлом
+  StringRef getArgument() const final { return "mamaeva-rem-pass"; }
 
   StringRef getDescription() const final {
     return "Replace remainder ops with div+mul+sub sequence";
   }
 
   void runOnOperation() override {
-    Operation *op = getOperation();
-    MLIRContext *context = op->getContext();
+    ModuleOp module = getOperation();
+    OpBuilder builder(module);
 
-    RewritePatternSet patterns(context);
-    patterns.add<RemLoweringPattern, URemLoweringPattern>(context);
+    module.walk([&](arith::RemSIOp op) {
+      expandRemainder(op, builder, /*isSigned=*/true);
+    });
 
-    if (failed(applyPatternsAndFoldGreedily(op, std::move(patterns)))) {
-      signalPassFailure();
-    }
+    module.walk([&](arith::RemUIOp op) {
+      expandRemainder(op, builder, /*isSigned=*/false);
+    });
   }
 };
 
@@ -79,8 +59,12 @@ MLIR_DEFINE_EXPLICIT_TYPE_ID(MamaevaRemPass)
 
 namespace {
 mlir::PassPluginLibraryInfo getMamaevaRemPassPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "rem_pass_Mamaeva_Olga_FIIT3", "1.0",
-          []() { mlir::PassRegistration<MamaevaRemPass>(); }};
+  return {MLIR_PLUGIN_API_VERSION,
+          "mamaeva-rem-pass-plugin", // Может отличаться от имени pass
+          "1.0", []() {
+            PassRegistration<MamaevaRemPass>(
+                []() { return std::make_unique<MamaevaRemPass>(); });
+          }};
 }
 } // namespace
 
