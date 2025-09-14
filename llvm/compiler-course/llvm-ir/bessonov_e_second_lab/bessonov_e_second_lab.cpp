d3343308a4b2dcb0e1308c829cdd7a436372272d
Egor Bessonov <egorkabessonov1@gmail.com>
2025-09-14 10:08:14 +0300

    4 com

--- llvm/compiler-course/llvm-ir/bessonov_e_second_lab/bessonov_e_second_lab.cpp
+++ llvm/compiler-course/llvm-ir/bessonov_e_second_lab/bessonov_e_second_lab.cpp
@@ -8,48 +8,38 @@
 using namespace llvm;
 
 namespace {
-
-  struct MarkPureFunctionsPass : PassInfoMixin<MarkPureFunctionsPass> {
-
-    static bool doesFunctionAccessMemory(const Function& F) {
-      for (const auto& BB : F) {
-        for (const auto& I : BB) {
-          if (I.mayReadOrWriteMemory())
-            return true;
-        }
+struct MarkPureFunctionsPass : PassInfoMixin<MarkPureFunctionsPass> {
+  static bool doesFunctionAccessMemory(const Function& F) {
+    for (const auto& BB : F) {
+      for (const auto& I : BB) {
+        if (I.mayReadOrWriteMemory())
+          return true;
       }
-      return false;
     }
-
-    PreservedAnalyses run(Function& F, FunctionAnalysisManager&) {
-      if (!F.hasFnAttribute("pure") && !doesFunctionAccessMemory(F)) {
-        F.addFnAttr("pure");
-      }
-
-      return PreservedAnalyses::all();
+    return false;
+  }
+  PreservedAnalyses run(Function& F, FunctionAnalysisManager&) {
+    if (!F.hasFnAttribute("pure") && !doesFunctionAccessMemory(F)) {
+      F.addFnAttr("pure");
     }
-
-    static bool isRequired() { return true; }
-  };
-
+    return PreservedAnalyses::all();
+  }
+  static bool isRequired() { return true; }
+};
 } // namespace
 
-extern "C" LLVM_ATTRIBUTE_WEAK::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION,
-    "MarkPureFunctionsPass",
-    "1.0",
-    [](PassBuilder& PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef Name, FunctionPassManager& FPM,
-           ArrayRef<PassBuilder::PipelineElement>) {
-          if (Name == "mark-pure") {
-            FPM.addPass(MarkPureFunctionsPass{});
-            return true;
-          }
-          return false;
-        }
-      );
-    }
-  };
+extern "C" LLVM_ATTRIBUTE_WEAK::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return { LLVM_PLUGIN_API_VERSION, "MarkPureFunctionsPass", "1.0",
+          [](PassBuilder& PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager& FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "mark-pure") {
+                    FPM.addPass(MarkPureFunctionsPass{});
+                    return true;
+                  }
+                  return false;
+                });
+          }};
 }
