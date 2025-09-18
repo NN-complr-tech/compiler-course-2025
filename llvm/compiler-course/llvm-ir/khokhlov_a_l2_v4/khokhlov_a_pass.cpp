588567e7e02e6a72c94cefec8808e001c229c6dc
pipidater2004 <chebureknabite@gmail.com>
2025-09-18 12:51:12 +0300

    clang-format

--- llvm/compiler-course/llvm-ir/khokhlov_a_l2_v4/khokhlov_a_pass.cpp
+++ llvm/compiler-course/llvm-ir/khokhlov_a_l2_v4/khokhlov_a_pass.cpp
@@ -1,7 +1,7 @@
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
@@ -13,60 +13,62 @@ struct DivisionToShiftPass : llvm::PassInfoMixin<DivisionToShiftPass> {
   llvm::PreservedAnalyses run(llvm::Function &func,
                               llvm::FunctionAnalysisManager &) {
     bool changed = false;
-    std::vector<llvm::Instruction*> toRemove;
-    
+    std::vector<llvm::Instruction *> toRemove;
+
     for (auto &BB : func) {
       for (auto &I : BB) {
         if (auto *divInst = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
-          if (divInst->getOpcode() == llvm::Instruction::SDiv || 
+          if (divInst->getOpcode() == llvm::Instruction::SDiv ||
               divInst->getOpcode() == llvm::Instruction::UDiv) {
-            
+
             llvm::Value *dividend = divInst->getOperand(0);
             llvm::Value *divisor = divInst->getOperand(1);
-            
-            if (auto *constDivisor = llvm::dyn_cast<llvm::ConstantInt>(divisor)) {
+
+            if (auto *constDivisor =
+                    llvm::dyn_cast<llvm::ConstantInt>(divisor)) {
               int64_t divisorValue = constDivisor->getSExtValue();
-              
+
               // Проверяем, является ли делитель положительной степенью двойки
-              if (divisorValue > 0 && (divisorValue & (divisorValue - 1)) == 0) {
+              if (divisorValue > 0 &&
+                  (divisorValue & (divisorValue - 1)) == 0) {
                 unsigned shiftAmount = llvm::Log2_64(divisorValue);
-                
+
                 llvm::IRBuilder<> builder(divInst);
-                
+
                 llvm::Value *shiftInst = nullptr;
                 if (divInst->getOpcode() == llvm::Instruction::SDiv) {
                   shiftInst = builder.CreateAShr(dividend, shiftAmount);
                 } else {
                   shiftInst = builder.CreateLShr(dividend, shiftAmount);
                 }
-                
+
                 divInst->replaceAllUsesWith(shiftInst);
                 toRemove.push_back(divInst);
-                
+
                 changed = true;
-                llvm::outs() << "Replaced division by " << divisorValue 
-                            << " with shift by " << shiftAmount << "\n";
-              }
-              else if (divisorValue == 1) {
+                llvm::outs() << "Replaced division by " << divisorValue
+                             << " with shift by " << shiftAmount << "\n";
+              } else if (divisorValue == 1) {
                 divInst->replaceAllUsesWith(dividend);
                 toRemove.push_back(divInst);
-                
+
                 changed = true;
-                llvm::outs() << "Replaced division by 1 with the operand itself\n";
+                llvm::outs()
+                    << "Replaced division by 1 with the operand itself\n";
               }
             }
           }
         }
       }
     }
-    
+
     for (auto *inst : toRemove) {
       if (inst->use_empty()) {
         inst->eraseFromParent();
       }
     }
-    
-    return changed ? llvm::PreservedAnalyses::none() 
+
+    return changed ? llvm::PreservedAnalyses::none()
                    : llvm::PreservedAnalyses::all();
   }
 
