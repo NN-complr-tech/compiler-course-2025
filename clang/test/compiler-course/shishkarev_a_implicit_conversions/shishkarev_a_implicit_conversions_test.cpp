0512b762af93114d0b20dc22cb6a739cbc68b977
shishkarev <and01072004@gmail.com>
2025-09-22 19:09:17 +0300

    made right tests

--- clang/test/compiler-course/shishkarev_a_implicit_conversions/shishkarev_a_implicit_conversions_test.cpp
+++ clang/test/compiler-course/shishkarev_a_implicit_conversions/shishkarev_a_implicit_conversions_test.cpp
@@ -1,13 +1,13 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/ImplicitConversionCounter_Shishkarev_Andrey_FIIT2_ClangAST%pluginext -plugin implicit_conversion_plugin -fsyntax-only %s 2>&1 | FileCheck %s
 
 // CHECK: Function `sum`
-// CHECK-NEXT: int -> float: 1
 // CHECK-NEXT: float -> double: 1
+// CHECK-NEXT: int -> float: 1
 
 // CHECK: Function `mul`
-// CHECK-NEXT: float -> int: 1
-// CHECK-NEXT: float -> double: 1
 // CHECK-NEXT: double -> int: 1
+// CHECK-NEXT: float -> double: 1
+// CHECK-NEXT: float -> int: 1
 
 // CHECK: Function `testMixedTypes`
 // CHECK-NEXT: int -> float: 2
