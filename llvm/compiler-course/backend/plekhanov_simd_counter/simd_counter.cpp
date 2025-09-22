a29aacdc127c54161f439db67c8a7ee073784a71
Dxppi <dxppi@vk.com>
2025-09-22 14:25:41 +0300

    fix

--- llvm/compiler-course/backend/plekhanov_simd_counter/simd_counter.cpp
+++ llvm/compiler-course/backend/plekhanov_simd_counter/simd_counter.cpp
@@ -5,111 +5,96 @@
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
-#include "llvm/CodeGen/MachineMemOperand.h"
 #include "llvm/CodeGen/MachineModuleInfo.h"
-#include "llvm/CodeGen/Register.h"
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/GlobalVariable.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 
-#define DEBUG_TYPE "instrument-simd"
-
 using namespace llvm;
 
 namespace {
 
-class InstrumentSimdPass : public MachineFunctionPass {
+class SimdInstrCounterPass : public MachineFunctionPass {
 public:
   static char ID;
-  InstrumentSimdPass() : MachineFunctionPass(ID) {}
+  SimdInstrCounterPass() : MachineFunctionPass(ID) {}
 
   bool doInitialization(Module &M) override {
     LLVMContext &Ctx = M.getContext();
-    Type *Int64Ty = Type::getInt64Ty(Ctx);
+    Type *Int64Type = Type::getInt64Ty(Ctx);
+    Constant *InitialValue = ConstantInt::get(Int64Type, 0);
 
-    if (M.getGlobalVariable("simd_counter")) {
-      return false;
-    }
+    auto *GlobalCounter =
+        cast<GlobalVariable>(M.getOrInsertGlobal("simd_counter", Int64Type));
+    GlobalCounter->setInitializer(InitialValue);
+    GlobalCounter->setLinkage(GlobalValue::ExternalLinkage);
+    GlobalCounter->setAlignment(Align(8));
 
-    Constant *Zero = ConstantInt::get(Int64Ty, 0);
-    GlobalVariable *CounterGV = new GlobalVariable(
-        M, Int64Ty, false, GlobalValue::ExternalLinkage, Zero, "simd_counter");
-    CounterGV->setAlignment(MaybeAlign(8));
     return true;
   }
 
   bool runOnMachineFunction(MachineFunction &MF) override {
-    MachineModuleInfo &MMI = MF.getMMI();
-    const Module *Mod = MMI.getModule();
-    const GlobalVariable *CounterGV = Mod->getGlobalVariable("simd_counter");
-
-    if (!CounterGV) {
-      errs() << "InstrumentSimdPass: simd_counter global variable not found.\n";
+    const Module *M = MF.getMMI().getModule();
+    const GlobalVariable *CounterVar = M->getGlobalVariable("simd_counter");
+    if (!CounterVar)
       return false;
-    }
 
-    const X86Subtarget &ST = MF.getSubtarget<X86Subtarget>();
-    const X86InstrInfo *TII = ST.getInstrInfo();
-    MachineRegisterInfo &MRI = MF.getRegInfo();
+    const X86Subtarget &Subtarget = MF.getSubtarget<X86Subtarget>();
+    const X86InstrInfo *InstrInfo = Subtarget.getInstrInfo();
+    MachineRegisterInfo &RegInfo = MF.getRegInfo();
 
-    bool Changed = false;
+    bool Modified = false;
 
-    for (auto &MBB : MF) {
-      for (auto MI_it = MBB.begin(), E = MBB.end(); MI_it != E; ++MI_it) {
-        MachineInstr &MI = *MI_it;
-        bool isSimd = false;
+    for (auto &Block : MF) {
+      unsigned VectorInstrs = 0;
 
-        for (const MachineOperand &MO : MI.operands()) {
-          if (!MO.isReg() || !MO.getReg())
-            continue;
+      for (auto &Instr : Block) {
+        for (const auto &Op : Instr.operands()) {
+          if (Op.isReg()) {
+            Register Reg = Op.getReg();
+            if (Reg.isVirtual()) {
+              const TargetRegisterClass *RC = RegInfo.getRegClass(Reg);
+              unsigned RCID = RC->getID();
 
-          unsigned Reg = MO.getReg();
-          if (!Register::isVirtualRegister(Reg))
-            continue;
-
-          const TargetRegisterClass *RC = MRI.getRegClass(Reg);
-          if (RC) {
-            unsigned RCID = RC->getID();
-            if (RCID == X86::VR128RegClassID || RCID == X86::VR256RegClassID ||
-                RCID == X86::VR512RegClassID) {
-              isSimd = true;
-              break;
+              if (RCID == X86::VR128RegClassID ||
+                  RCID == X86::VR256RegClassID ||
+                  RCID == X86::VR512RegClassID) {
+                ++VectorInstrs;
+                break;
+              }
             }
           }
         }
-
-        if (!isSimd)
-          continue;
-
-        Changed = true;
-        DebugLoc DL = MI.getDebugLoc();
-
-        MachineInstrBuilder MIB =
-            BuildMI(MBB, MI_it, DL, TII->get(X86::ADD64mi8));
-
-        MIB.addReg(X86::RIP, RegState::Implicit);
-        MIB.addImm(1);
-        MIB.addReg(0, RegState::Implicit);
-        MIB.addGlobalAddress(CounterGV, 0);
-        MIB.addReg(0, RegState::Implicit);
-        MIB.addImm(1);
-
-        MachineMemOperand *MMO = MF.getMachineMemOperand(
-            MachinePointerInfo(CounterGV),
-            MachineMemOperand::MOLoad | MachineMemOperand::MOStore |
-                MachineMemOperand::MOVolatile,
-            8, Align(8));
-        MIB.addMemOperand(MMO);
       }
+
+      if (VectorInstrs == 0)
+        continue;
+
+      auto InsertPos = Block.getFirstTerminator();
+      DebugLoc Loc;
+      if (InsertPos != Block.end())
+        Loc = InsertPos->getDebugLoc();
+
+      BuildMI(Block, InsertPos, Loc, InstrInfo->get(X86::ADD64mi32))
+          .addReg(X86::RIP)               
+          .addImm(1)                       
+          .addReg(0)                      
+          .addGlobalAddress(CounterVar)    
+          .addReg(0)                       
+          .addImm(VectorInstrs);           
+
+      Modified = true;
     }
-    return Changed;
+
+    return Modified;
   }
 };
 
-char InstrumentSimdPass::ID = 0;
+char SimdInstrCounterPass::ID = 0;
 
 } // namespace
 
-static llvm::RegisterPass<InstrumentSimdPass>
-    X("simd-pass", "Instrument SIMD instructions", false, false);
+static RegisterPass<SimdInstrCounterPass>
+    X("simd-counter", "Counts SIMD instructions and instruments counter",
+      false, false);
