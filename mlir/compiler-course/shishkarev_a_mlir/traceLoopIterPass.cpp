71ea66b42deb0a81374e6c0737a7de8a09198fc8
shishkarev <and01072004@gmail.com>
2025-09-24 17:39:01 +0300

    fix from review

--- mlir/compiler-course/shishkarev_a_mlir/traceLoopIterPass.cpp
+++ mlir/compiler-course/shishkarev_a_mlir/traceLoopIterPass.cpp
@@ -24,59 +24,46 @@ struct TraceLoopPass
         .insert<affine::AffineDialect, scf::SCFDialect, func::FuncDialect>();
   }
 
+private:
+  void createTraceFunction(ModuleOp module, OpBuilder &builder, 
+                          StringRef functionName) {
+    if (!module.lookupSymbol<func::FuncOp>(functionName)) {
+      Location loc = module.getLoc();
+      builder.setInsertionPointToStart(module.getBody());
+      auto func = builder.create<func::FuncOp>(
+          loc, functionName, builder.getFunctionType({}, {}));
+      func.setSymVisibility("private");
+    }
+  }
+
+  void instrumentLoopBody(OpBuilder &builder, Block &body, Location loopLoc) {
+    builder.setInsertionPointToStart(&body);
+    builder.create<func::CallOp>(loopLoc, "trace_loop_iter_begin",
+                                 TypeRange{}, ValueRange{});
+    
+    Operation *term = body.getTerminator();
+    builder.setInsertionPoint(term);
+    builder.create<func::CallOp>(loopLoc, "trace_loop_iter_end",
+                                 TypeRange{}, ValueRange{});
+  }
+
+public:
   void runOnOperation() override {
     ModuleOp module = getOperation();
     OpBuilder builder(module.getContext());
-    Location loc = module.getLoc();
 
-    if (!module.lookupSymbol<func::FuncOp>("trace_loop_iter_begin")) {
-      builder.setInsertionPointToStart(module.getBody());
-      auto begin = builder.create<func::FuncOp>(
-          loc, "trace_loop_iter_begin", builder.getFunctionType({}, {}));
-      begin.setSymVisibility("private");
-    }
-    if (!module.lookupSymbol<func::FuncOp>("trace_loop_iter_end")) {
-      builder.setInsertionPointToStart(module.getBody());
-      auto end = builder.create<func::FuncOp>(loc, "trace_loop_iter_end",
-                                              builder.getFunctionType({}, {}));
-      end.setSymVisibility("private");
-    }
+    createTraceFunction(module, builder, "trace_loop_iter_begin");
+    createTraceFunction(module, builder, "trace_loop_iter_end");
 
     module.walk([&](Operation *op) {
-      Location loopLoc = op->getLoc();
       OpBuilder::InsertionGuard guard(builder);
-
+      
       if (auto forOp = dyn_cast<affine::AffineForOp>(op)) {
-        Block &body = *forOp.getBody();
-        builder.setInsertionPointToStart(&body);
-        builder.create<func::CallOp>(loopLoc, "trace_loop_iter_begin",
-                                     TypeRange{}, ValueRange{});
-        Operation *term = body.getTerminator();
-        builder.setInsertionPoint(term);
-        builder.create<func::CallOp>(loopLoc, "trace_loop_iter_end",
-                                     TypeRange{}, ValueRange{});
-      }
-
-      else if (auto scfFor = dyn_cast<scf::ForOp>(op)) {
-        Block &body = *scfFor.getBody();
-        builder.setInsertionPointToStart(&body);
-        builder.create<func::CallOp>(loopLoc, "trace_loop_iter_begin",
-                                     TypeRange{}, ValueRange{});
-        Operation *term = body.getTerminator();
-        builder.setInsertionPoint(term);
-        builder.create<func::CallOp>(loopLoc, "trace_loop_iter_end",
-                                     TypeRange{}, ValueRange{});
-      }
-
-      else if (auto whileOp = dyn_cast<scf::WhileOp>(op)) {
-        Block &body = whileOp.getAfter().front();
-        builder.setInsertionPointToStart(&body);
-        builder.create<func::CallOp>(loopLoc, "trace_loop_iter_begin",
-                                     TypeRange{}, ValueRange{});
-        Operation *term = body.getTerminator();
-        builder.setInsertionPoint(term);
-        builder.create<func::CallOp>(loopLoc, "trace_loop_iter_end",
-                                     TypeRange{}, ValueRange{});
+        instrumentLoopBody(builder, *forOp.getBody(), op->getLoc());
+      } else if (auto scfFor = dyn_cast<scf::ForOp>(op)) {
+        instrumentLoopBody(builder, *scfFor.getBody(), op->getLoc());
+      } else if (auto whileOp = dyn_cast<scf::WhileOp>(op)) {
+        instrumentLoopBody(builder, whileOp.getAfter().front(), op->getLoc());
       }
     });
   }
