3db14dfea0e8d8f9ba0977e1f7c1d33e49ac57bb
Vadim <vivasuk683@gmail.com>
2025-11-11 02:46:06 +0300

    'fix'

--- llvm/compiler-course/backend/IvashchukVA_lab3_var2/IvashchukVA_lab3_var2.cpp
+++ llvm/compiler-course/backend/IvashchukVA_lab3_var2/IvashchukVA_lab3_var2.cpp
@@ -14,7 +14,6 @@ namespace {
 
 struct VectorCounterPass : public PassInfoMixin<VectorCounterPass> {
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-    // Create or get the global counter variable
     GlobalVariable *VectorCounter =
         M.getNamedGlobal("vector_instructions_counter");
     if (!VectorCounter) {
@@ -25,14 +24,6 @@ struct VectorCounterPass : public PassInfoMixin<VectorCounterPass> {
           "vector_instructions_counter");
     }
 
-    Function *IncrementFunc = M.getFunction("increment_vector_counter");
-    if (!IncrementFunc) {
-      FunctionType *FT =
-          FunctionType::get(Type::getVoidTy(M.getContext()), false);
-      IncrementFunc = Function::Create(FT, GlobalValue::ExternalLinkage,
-                                       "increment_vector_counter", &M);
-    }
-
     bool Modified = false;
     IRBuilder<> Builder(M.getContext());
 
@@ -41,10 +32,12 @@ struct VectorCounterPass : public PassInfoMixin<VectorCounterPass> {
         continue;
 
       for (auto &BB : F) {
-        for (auto &I : BB) {
-          if (isVectorInstruction(&I)) {
-            // Insert counter increment after the vector instruction
-            Builder.SetInsertPoint(&BB, ++I.getIterator());
+        for (auto I = BB.begin(); I != BB.end();) {
+          Instruction *CurrentI = &*I;
+          ++I;
+
+          if (isVectorInstruction(CurrentI)) {
+            Builder.SetInsertPoint(&BB, I);
 
             LoadInst *Load = Builder.CreateLoad(
                 IntegerType::getInt64Ty(M.getContext()), VectorCounter);
@@ -54,21 +47,20 @@ struct VectorCounterPass : public PassInfoMixin<VectorCounterPass> {
                 ConstantInt::get(IntegerType::getInt64Ty(M.getContext()), 1));
 
             Builder.CreateStore(Increment, VectorCounter);
-
             Modified = true;
           }
         }
       }
     }
 
-    // Add print function to output the counter value
-    addPrintFunction(M, VectorCounter);
-
     return Modified ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
 
 private:
   bool isVectorInstruction(Instruction *I) {
+    if (I->getType()->isVectorTy()) {
+      return true;
+    }
 
     for (auto &Op : I->operands()) {
       if (Op->getType()->isVectorTy()) {
@@ -76,48 +68,7 @@ private:
       }
     }
 
-    if (I->getType()->isVectorTy()) {
-      return true;
-    }
-
-    if (auto *CI = dyn_cast<CallInst>(I)) {
-      if (Function *F = CI->getCalledFunction()) {
-        StringRef Name = F->getName();
-        if (Name.startswith("llvm.") &&
-            (Name.contains("vector") || Name.contains("simd") ||
-             Name.contains("mask") || Name.contains("gather") ||
-             Name.contains("scatter"))) {
-          return true;
-        }
-      }
-    }
-
     return false;
-  }
-
-  void addPrintFunction(Module &M, GlobalVariable *Counter) {
-    FunctionType *PrintfType =
-        FunctionType::get(IntegerType::getInt32Ty(M.getContext()),
-                          PointerType::getUnqual(M.getContext()), true);
-    Function *PrintfFunc = M.getOrInsertFunction("printf", PrintfType);
-
-    FunctionType *FT =
-        FunctionType::get(Type::getVoidTy(M.getContext()), false);
-    Function *PrintCounterFunc = Function::Create(
-        FT, GlobalValue::ExternalLinkage, "print_vector_counter", &M);
-
-    BasicBlock *BB =
-        BasicBlock::Create(M.getContext(), "entry", PrintCounterFunc);
-    IRBuilder<> Builder(BB);
-
-    Constant *FormatStr = Builder.CreateGlobalStringPtr(
-        "Total vector instructions executed: %lld\n");
-
-    LoadInst *CounterVal =
-        Builder.CreateLoad(IntegerType::getInt64Ty(M.getContext()), Counter);
-
-    Builder.CreateCall(PrintfFunc, {FormatStr, CounterVal});
-    Builder.CreateRetVoid();
   }
 };
 
@@ -125,8 +76,7 @@ private:
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION,
-          "IvashchukVA Lab3 - Vector Instructions Counter", LLVM_VERSION_STRING,
+  return {LLVM_PLUGIN_API_VERSION, "IvashchukVA Lab3", LLVM_VERSION_STRING,
           [](PassBuilder &PB) {
             PB.registerPipelineParsingCallback(
                 [](StringRef Name, ModulePassManager &MPM,
