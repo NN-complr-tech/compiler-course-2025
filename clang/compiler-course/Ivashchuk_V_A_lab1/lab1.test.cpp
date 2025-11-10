80b52c63caa4b0d2a99254282eb313b31737f9a7
Vadim <vivasuk683@gmail.com>
2025-11-11 00:55:17 +0300

    'fix'

---
+++ clang/compiler-course/Ivashchuk_V_A_lab1/lab1.test.cpp
@@ -0,0 +1,13 @@
+// RUN: %clang -fplugin=%llvmshlibdir/IvashchukVA_lab1%pluginext -Xclang -plugin
+// -Xclang IvashchukVA_lab1_2var %s -S -emit-llvm -o - | FileCheck %s
+
+void test_function(int unused, int used) {
+  [[maybe_unused]] int unused_var = 42;
+  int used_var = 43;
+  // CHECK: @_Z13test_functioni
+}
+
+int main() {
+  test_function(1, 2);
+  return 0;
+}
