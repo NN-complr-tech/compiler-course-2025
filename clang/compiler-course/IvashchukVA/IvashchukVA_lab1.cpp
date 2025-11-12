b1edd9859a24adb31ac0387f2aba27971bfdec28
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-12 18:25:13 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -2,7 +2,6 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
-#include "clang/Rewrite/Core/Rewriter.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
@@ -10,10 +9,10 @@ using namespace clang;
 class AddMaybeUnusedVisitor
     : public RecursiveASTVisitor<AddMaybeUnusedVisitor> {
 private:
-  Rewriter &TheRewriter;
+  DiagnosticsEngine &Diag;
 
 public:
-  explicit AddMaybeUnusedVisitor(Rewriter &R) : TheRewriter(R) {}
+  explicit AddMaybeUnusedVisitor(DiagnosticsEngine &D) : Diag(D) {}
 
   bool VisitVarDecl(VarDecl *VD) {
     if (!VD->hasInit() || VD->isImplicit() || VD->isFunctionOrMethodVarDecl()) {
@@ -22,50 +21,6 @@ public:
 
     StringRef Name = VD->getName();
     if (Name.contains("unused")) {
-      SourceLocation Loc = VD->getBeginLoc();
-      if (Loc.isValid()) {
-        TheRewriter.InsertText(Loc, "[[maybe_unused]] ");
-      }
-    }
-
-    return true;
-  }
-};
-
-class AddMaybeUnusedConsumer : public ASTConsumer {
-private:
-  Rewriter TheRewriter;
-  AddMaybeUnusedVisitor Visitor;
-
-public:
-  explicit AddMaybeUnusedConsumer(CompilerInstance &CI)
-      : TheRewriter(CI.getSourceManager(), CI.getLangOpts()),
-        Visitor(TheRewriter) {}
-
-  void HandleTranslationUnit(ASTContext &Context) override {
-    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-    TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
-        .write(llvm::outs());
-  }
-};
-
-class AddMaybeUnusedAction : public PluginASTAction {
-public:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
-                                                 StringRef InFile) override {
-    return std::make_unique<AddMaybeUnusedConsumer>(CI);
-  }
-
-  bool ParseArgs(const CompilerInstance &CI,
-                 const std::vector<std::string> &Args) override {
-    return true;
-  }
-
-  PluginASTAction::ActionType getActionType() override {
-    return AddAfterMainAction;
-  }
-};
-
-static FrontendPluginRegistry::Add<AddMaybeUnusedAction>
-    X("lab1_IvashchukVA_FIIT2_ClangAST",
-      "Adds [[maybe_unused]] to variables containing 'unused'");
+      Diag.Report(VD->getLocation(), Diag.getCustomDiagID(
+                                         DiagnosticsEngine::Remark,
+                                         "Found variable with 'un
