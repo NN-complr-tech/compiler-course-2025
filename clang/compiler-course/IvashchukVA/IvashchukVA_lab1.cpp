0cf0b9bf96d8fcfa42d73023763fa08a02e5cd62
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-12 18:12:44 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -2,13 +2,19 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Rewrite/Core/Rewriter.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
 
 class AddMaybeUnusedVisitor
     : public RecursiveASTVisitor<AddMaybeUnusedVisitor> {
+private:
+  Rewriter &TheRewriter;
+
 public:
+  explicit AddMaybeUnusedVisitor(Rewriter &R) : TheRewriter(R) {}
+
   bool VisitVarDecl(VarDecl *VD) {
     if (!VD->hasInit() || VD->isImplicit() || VD->isFunctionOrMethodVarDecl()) {
       return true;
@@ -16,19 +22,30 @@ public:
 
     StringRef Name = VD->getName();
     if (Name.contains("unused")) {
-      llvm::errs() << "MODIFIED: [[maybe_unused]] " << Name << "\n";
+      SourceLocation Loc = VD->getBeginLoc();
+      if (Loc.isValid()) {
+        TheRewriter.InsertText(Loc, "[[maybe_unused]] ");
+      }
     }
+
     return true;
   }
 };
 
 class AddMaybeUnusedConsumer : public ASTConsumer {
 private:
+  Rewriter TheRewriter;
   AddMaybeUnusedVisitor Visitor;
 
 public:
+  explicit AddMaybeUnusedConsumer(CompilerInstance &CI)
+      : TheRewriter(CI.getSourceManager(), CI.getLangOpts()),
+        Visitor(TheRewriter) {}
+
   void HandleTranslationUnit(ASTContext &Context) override {
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+    TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
   }
 };
 
@@ -36,7 +53,7 @@ class AddMaybeUnusedAction : public PluginASTAction {
 public:
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef InFile) override {
-    return std::make_unique<AddMaybeUnusedConsumer>();
+    return std::make_unique<AddMaybeUnusedConsumer>(CI);
   }
 
   bool ParseArgs(const CompilerInstance &CI,
