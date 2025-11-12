ad0f4b9f4e60e80603905ceda8dd335461e496e2
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-12 11:58:11 +0300

    Update IvashchukVA_lab4.cpp

--- mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
+++ mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
@@ -1,47 +1,34 @@
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 using namespace mlir;
 
 namespace {
-struct CallCounterPass
-    : public PassWrapper<CallCounterPass, OperationPass<ModuleOp>> {
+struct CallCounterPass : public PassWrapper<CallCounterPass, OperationPass<ModuleOp>> {
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(CallCounterPass)
 
-  StringRef getArgument() const final {
-    return "CallCounterPass_IvashchukVA_FIIT2_MLIR";
-  }
-
-  StringRef getDescription() const final {
-    return "Count function calls in MLIR";
-  }
+  StringRef getArgument() const final { return "CallCounterPass_IvashchukVA_FIIT2_MLIR"; }
+  StringRef getDescription() const final { return "Count function calls in MLIR"; }
 
   void runOnOperation() override {
     ModuleOp module = getOperation();
-
+    
     for (auto func : module.getOps<func::FuncOp>()) {
       int callCount = 0;
-
+      
       func.walk([&](Operation *op) {
         if (isa<func::CallOp>(op)) {
           callCount++;
         }
       });
-
+      
       if (callCount > 0) {
-        func->setAttr(
-            "call_count",
-            IntegerAttr::get(IntegerType::get(module.getContext(), 64),
-                             callCount));
+        func->setAttr("call_count", IntegerAttr::get(
+            IntegerType::get(module.getContext(), 64), callCount));
       }
     }
   }
 };
 } // namespace
-
-static mlir::PassRegistration<CallCounterPass> unused(
-    "CallCounterPass_IvashchukVA_FIIT2_MLIR",
-    "Count function calls in MLIR");
