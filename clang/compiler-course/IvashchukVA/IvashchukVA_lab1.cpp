9c2eb8887f1e2ade850229d76dce2918b33cb7da
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-12 17:10:14 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -2,13 +2,19 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Rewrite/Core/Rewriter.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
 
 class AddMaybeUnusedVisitor
     : public RecursiveASTVisitor<AddMaybeUnusedVisitor> {
+private:
+  Rewriter &TheRewriter;
+
 public:
+  explicit AddMaybeUnusedVisitor(Rewriter &R) : TheRewriter(R) {}
+
   bool VisitVarDecl(VarDecl *VD) {
     if (!VD->hasInit() || VD->isImplicit() || VD->isFunctionOrMethodVarDecl()) {
       return true;
@@ -16,7 +22,10 @@ public:
 
     StringRef Name = VD->getName();
     if (Name.contains("unused")) {
-      llvm::outs() << "Found variable: " << Name << "\n";
+      SourceLocation Loc = VD->getBeginLoc();
+      if (Loc.isValid()) {
+        TheRewriter.InsertText(Loc, "[[maybe_unused]] ");
+      }
     }
 
     return true;
@@ -25,31 +34,14 @@ public:
 
 class AddMaybeUnusedConsumer : public ASTConsumer {
 private:
+  Rewriter TheRewriter;
   AddMaybeUnusedVisitor Visitor;
 
 public:
+  explicit AddMaybeUnusedConsumer(CompilerInstance &CI)
+      : TheRewriter(CI.getSourceManager(), CI.getLangOpts()),
+        Visitor(TheRewriter) {}
+
   void HandleTranslationUnit(ASTContext &Context) override {
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-  }
-};
-
-class AddMaybeUnusedAction : public PluginASTAction {
-public:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
-                                                 StringRef InFile) override {
-    return std::make_unique<AddMaybeUnusedConsumer>();
-  }
-
-  bool ParseArgs(const CompilerInstance &CI,
-                 const std::vector<std::string> &Args) override {
-    return true;
-  }
-
-  PluginASTAction::ActionType getActionType() override {
-    return AddAfterMainAction;
-  }
-};
-
-static FrontendPluginRegistry::Add<AddMaybeUnusedAction>
-    X("lab1_IvashchukVA_FIIT2_ClangAST",
-      "Adds [[maybe_unused]] to variables containing 'unused'");
+    TheRewriter.getEditBuffer
