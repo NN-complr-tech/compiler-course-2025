f9eb58816ff0800016b8a29cf445eecc9cb8ac17
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-13 12:05:14 +0300

    Update IvashchukVA_lab4.cpp

--- mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
+++ mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
@@ -1,7 +1,15 @@
-#include "mlir/Dialect/Func/IR/FuncOps.h"
+//===- IvashchukVA_lab4.cpp - MLIR Call Counter Pass ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 using namespace mlir;
 
@@ -11,8 +19,9 @@ struct CallCounterPass
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(CallCounterPass)
 
   StringRef getArgument() const final { return "call-counter"; }
+
   StringRef getDescription() const final {
-    return "Count function calls in MLIR";
+    return "Count function calls and add call_count attribute";
   }
 
   void runOnOperation() override {
@@ -36,3 +45,7 @@ struct CallCounterPass
   }
 };
 } // namespace
+
+void registerCallCounterPass() {
+  PassRegistration<CallCounterPass>();
+}
