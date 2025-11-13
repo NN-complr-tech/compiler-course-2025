582d964c39ad8de68316165acad4f82ddb711aa6
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-13 15:12:26 +0300

    Update IvashchukVA_lab4.cpp

--- mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
+++ mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
@@ -1,21 +1,18 @@
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Pass/PassRegistry.h"
 
 using namespace mlir;
 
 namespace {
 struct CallCounterPass : public PassWrapper<CallCounterPass,
                                            OperationPass<ModuleOp>> {
-  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(CallCounterPass)
-
   StringRef getArgument() const final { return "call-counter"; }
   StringRef getDescription() const final { return "Count function calls"; }
 
   void runOnOperation() override {
     ModuleOp module = getOperation();
-    
+
     for (auto func : module.getOps<func::FuncOp>()) {
       int count = 0;
       func.walk([&](Operation *op) {
@@ -23,11 +20,11 @@ struct CallCounterPass : public PassWrapper<CallCounterPass,
           count++;
         }
       });
-      
+
       if (count > 0) {
-        func->setAttr("call_count", 
-                     IntegerAttr::get(IntegerType::get(&getContext(), 64), 
-                                     count));
+        func->setAttr("call_count",
+                      IntegerAttr::get(IntegerType::get(&getContext(), 64),
+                                       count));
       }
     }
   }
@@ -36,14 +33,4 @@ struct CallCounterPass : public PassWrapper<CallCounterPass,
 
 void registerCallCounterPass() {
   PassRegistration<CallCounterPass>();
-}
-
-extern "C" LLVM_ATTRIBUTE_WEAK ::mlir::PassPluginLibraryInfo
-mlirGetPassPluginInfo() {
-  return {
-    MLIR_PLUGIN_API_VERSION, "CallCounterPass", "v0.1",
-    [](mlir::PassManager &pm) {
-      pm.addPass(std::make_unique<CallCounterPass>());
-    }
-  };
 }
