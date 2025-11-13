d0c0184c5bddcc0825d489ad62cb931f3f066a6f
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-13 12:10:30 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -2,13 +2,23 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Rewrite/Core/Rewriter.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
 
+namespace {
+
 class AddMaybeUnusedVisitor
     : public RecursiveASTVisitor<AddMaybeUnusedVisitor> {
+private:
+  Rewriter &RewriterRef;
+  ASTContext *ContextPtr;
+
 public:
+  explicit AddMaybeUnusedVisitor(Rewriter &R, ASTContext *Ctx)
+      : RewriterRef(R), ContextPtr(Ctx) {}
+
   bool VisitVarDecl(VarDecl *VD) {
     if (!VD->hasInit() || VD->isImplicit() || VD->isFunctionOrMethodVarDecl()) {
       return true;
@@ -16,19 +26,39 @@ public:
 
     StringRef Name = VD->getName();
     if (Name.contains("unused")) {
-      llvm::errs() << "FOUND: " << Name << "\n";
+      SourceLocation Loc = VD->getBeginLoc();
+      if (Loc.isValid()) {
+        RewriterRef.InsertText(Loc, "[[maybe_unused]] ");
+      }
     }
+
     return true;
   }
 };
 
 class AddMaybeUnusedConsumer : public ASTConsumer {
 private:
+  Rewriter TheRewriter;
   AddMaybeUnusedVisitor Visitor;
 
 public:
+  explicit AddMaybeUnusedConsumer(CompilerInstance &CI)
+      : TheRewriter(CI.getSourceManager(), CI.getLangOpts()),
+        Visitor(TheRewriter, &CI.getASTContext()) {}
+
   void HandleTranslationUnit(ASTContext &Context) override {
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+
+    if (TheRewriter.getNumModifications() > 0) {
+      std::error_code EC;
+      llvm::raw_fd_ostream OS(
+          TheRewriter.getSourceMgr().getMainFileID().getName(),
+          EC, llvm::sys::fs::OF_Text);
+      if (!EC) {
+        TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
+            .write(OS);
+      }
+    }
   }
 };
 
@@ -36,7 +66,7 @@ class AddMaybeUnusedAction : public PluginASTAction {
 public:
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef InFile) override {
-    return std::make_unique<AddMaybeUnusedConsumer>();
+    return std::make_unique<AddMaybeUnusedConsumer>(CI);
   }
 
   bool ParseArgs(const CompilerInstance &CI,
@@ -48,6 +78,8 @@ public:
     return AddAfterMainAction;
   }
 };
+
+} // namespace
 
 static FrontendPluginRegistry::Add<AddMaybeUnusedAction>
     X("lab1_IvashchukVA_FIIT2_ClangAST",
