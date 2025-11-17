cdd9c812f93440cb9c0d6313e16400777c842818
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-17 21:43:11 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -1,14 +1,42 @@
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
 
 namespace {
 
+class AddMaybeUnusedVisitor
+    : public RecursiveASTVisitor<AddMaybeUnusedVisitor> {
+public:
+  bool VisitVarDecl(VarDecl *VD) {
+    if (!VD->hasInit() || VD->isImplicit() || VD->isFunctionOrMethodVarDecl()) {
+      return true;
+    }
+
+    StringRef Name = VD->getName();
+    if (Name.contains("unused")) {
+      // Просто возвращаем true, ничего не выводим
+    }
+    return true;
+  }
+};
+
+class AddMaybeUnusedConsumer : public ASTConsumer {
+public:
+  void HandleTranslationUnit(ASTContext &Context) override {
+    AddMaybeUnusedVisitor Visitor;
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+};
+
 class AddMaybeUnusedAction : public PluginASTAction {
 public:
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef InFile) override {
-    return std::make_unique<ASTConsumer>();
+    return std::make_unique<AddMaybeUnusedConsumer>();
   }
 
   bool ParseArgs(const CompilerInstance &CI,
