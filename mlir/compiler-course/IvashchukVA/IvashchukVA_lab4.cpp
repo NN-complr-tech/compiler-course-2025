d89f26a3a7bebd7b2972d3b5749f75cecaa92386
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-17 18:25:01 +0300

    Update IvashchukVA_lab4.cpp

--- mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
+++ mlir/compiler-course/IvashchukVA/IvashchukVA_lab4.cpp
@@ -1,48 +1,18 @@
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Tools/Plugins/PassPlugin.h"
+// RUN: mlir-opt -load-pass-plugin=%mlir_lib_dir/CallCounterPass_IvashchukVA_FIIT2_MLIR%shlibext --pass-pipeline="builtin.module(call-counter)" %s | FileCheck %s
 
-using namespace mlir;
-
-namespace {
-struct CallCounterPass
-    : public PassWrapper<CallCounterPass, OperationPass<ModuleOp>> {
-  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(CallCounterPass)
-
-  StringRef getArgument() const final { return "call-counter"; }
-  StringRef getDescription() const final { return "Count function calls"; }
-
-  void runOnOperation() override {
-    ModuleOp module = getOperation();
-
-    for (auto func : module.getOps<func::FuncOp>()) {
-      int count = 0;
-      func.walk([&](Operation *op) {
-        if (isa<func::CallOp>(op)) {
-          count++;
-        }
-      });
-
-      if (count > 0) {
-        func->setAttr(
-            "call_count",
-            IntegerAttr::get(IntegerType::get(&getContext(), 64), count));
-      }
-    }
-  }
-};
-} // namespace
-
-MLIR_DECLARE_EXPLICIT_TYPE_ID(CallCounterPass)
-MLIR_DEFINE_EXPLICIT_TYPE_ID(CallCounterPass)
-
-mlir::PassPluginLibraryInfo getCallCounterPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "CallCounterPass", "1.0",
-          []() { PassRegistration<CallCounterPass>(); }};
+func.func @foo() -> () {
+  return
 }
 
-extern "C" LLVM_ATTRIBUTE_WEAK mlir::PassPluginLibraryInfo
-mlirGetPassPluginInfo() {
-  return getCallCounterPluginInfo();
+func.func @bar() -> () {
+  return
 }
+
+func.func @main() -> () {
+  call @foo() : () -> ()
+  call @foo() : () -> ()
+  call @bar() : () -> ()
+  return
+}
+
+// CHECK: func.func @main() attributes {call_count = 3}
