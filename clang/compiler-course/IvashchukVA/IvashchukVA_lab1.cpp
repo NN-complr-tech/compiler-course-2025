5440f961e745391e008be116cb71a5a7155cbe0b
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-18 22:41:10 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -1,15 +1,58 @@
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace clang;
 
 namespace {
 
+class AddMaybeUnusedVisitor : public RecursiveASTVisitor<AddMaybeUnusedVisitor> {
+private:
+  Rewriter &rewriter;
+
+public:
+  explicit AddMaybeUnusedVisitor(Rewriter &RW) : rewriter(RW) {}
+
+  bool VisitVarDecl(VarDecl *VD) {
+    if (!VD->hasInit() || VD->isImplicit() || VD->isFunctionOrMethodVarDecl()) {
+      return true;
+    }
+
+    StringRef Name = VD->getName();
+    if (Name.contains("unused")) {
+      SourceLocation loc = VD->getBeginLoc();
+      if (loc.isValid()) {
+        rewriter.InsertTextBefore(loc, "[[maybe_unused]] ");
+      }
+    }
+    return true;
+  }
+};
+
+class AddMaybeUnusedConsumer : public ASTConsumer {
+private:
+  AddMaybeUnusedVisitor visitor;
+
+public:
+  explicit AddMaybeUnusedConsumer(Rewriter &R) : visitor(R) {}
+
+  void HandleTranslationUnit(ASTContext &Context) override {
+    visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+};
+
 class AddMaybeUnusedAction : public PluginASTAction {
+private:
+  Rewriter rewriter;
+
 public:
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef InFile) override {
-    // Возвращаем nullptr - самый безопасный вариант
-    return nullptr;
+    rewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    return std::make_unique<AddMaybeUnusedConsumer>(rewriter);
   }
 
   bool ParseArgs(const CompilerInstance &CI,
@@ -17,12 +60,14 @@ public:
     return true;
   }
 
-  PluginASTAction::ActionType getActionType() override {
-    return CmdlineBeforeMainAction;
+  void EndSourceFileAction() override {
+    rewriter.getEditBuffer(rewriter.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
   }
 };
 
 } // namespace
 
 static FrontendPluginRegistry::Add<AddMaybeUnusedAction>
-    X("lab1_IvashchukVA_FIIT2_ClangAST", "Lab1 plugin");
+    X("lab1_IvashchukVA_FIIT2_ClangAST",
+      "Adds [[maybe_unused]] to variables containing 'unused'");
