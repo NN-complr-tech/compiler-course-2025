eba92a1ab6341ab731235f7416973d2f35895901
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-18 17:14:35 +0300

    Update IvashchukVA_lab1.cpp

--- clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
+++ clang/compiler-course/IvashchukVA/IvashchukVA_lab1.cpp
@@ -1,14 +1,28 @@
+#include "clang/AST/ASTConsumer.h"
+#include "clang/Basic/LLVM.h"
+#include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 
 using namespace clang;
 
 namespace {
 
-class EmptyAction : public PluginASTAction {
+class AddMaybeUnusedConsumer : public ASTConsumer {
 public:
+  AddMaybeUnusedConsumer() = default;
+  ~AddMaybeUnusedConsumer() override = default;
+  
+  void HandleTranslationUnit(ASTContext &Context) override {}
+};
+
+class AddMaybeUnusedAction : public PluginASTAction {
+public:
+  AddMaybeUnusedAction() = default;
+  ~AddMaybeUnusedAction() override = default;
+  
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef InFile) override {
-    return std::make_unique<ASTConsumer>();
+    return std::make_unique<AddMaybeUnusedConsumer>();
   }
 
   bool ParseArgs(const CompilerInstance &CI,
@@ -23,5 +37,5 @@ public:
 
 } // namespace
 
-static FrontendPluginRegistry::Add<EmptyAction>
-    X("lab1_IvashchukVA_FIIT2_ClangAST", "Empty plugin");
+static FrontendPluginRegistry::Add<AddMaybeUnusedAction>
+    X("lab1_IvashchukVA_FIIT2_ClangAST", "Lab1 plugin");
