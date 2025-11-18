863903a925cf530d189faef79a82ee8ae0fe20fc
Vadim Ivashchuk <57287478+vadimka683@users.noreply.github.com>
2025-11-18 17:20:12 +0300

    Update lab1.test.cpp

--- clang/test/compiler-course/IvashchukVA/lab1.test.cpp
+++ clang/test/compiler-course/IvashchukVA/lab1.test.cpp
@@ -1,5 +1,9 @@
-// RUN: %clang_cc1 -load %llvmshlibdir/lab1_IvashchukVA_FIIT2_ClangAST%pluginext -plugin lab1_IvashchukVA_FIIT2_ClangAST %s
+// RUN: %clang_cc1 -load %llvmshlibdir/lab1_IvashchukVA_FIIT2_ClangAST%pluginext -plugin lab1_IvashchukVA_FIIT2_ClangAST %s 2>&1 | FileCheck %s
 
 int main() {
+  int unused_var = 42;
+  int used_var = 43;
   return 0;
 }
+
+// CHECK: Found: unused_var
